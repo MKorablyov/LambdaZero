bd8303be5e168583e9dff695d7c7b8545ec353ac
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-04-23 14:15:00 -0400

    Create path variable to the data.

--- LambdaZero/datasets/brutal_dock/__init__.py
+++ LambdaZero/datasets/brutal_dock/__init__.py
@@ -1,3 +1,9 @@
 from pathlib import Path
 
 BRUTAL_DOCK_BASE_DIR = Path(__file__).parent
+
+# TODO: this should be defined in an init file closer to the actual root dir.
+# We limit ourselves to coding here for now.
+ROOT_DIR = BRUTAL_DOCK_BASE_DIR.parent.parent.parent
+
+BRUTAL_DOCK_DATA_DIR = ROOT_DIR.joinpath("Datasets/brutal_dock/")
