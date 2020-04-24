edcf6c060ceb6beb6aa1133283030215cb8f1b3a
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-04-24 14:23:01 -0400

    Fix import and internal variable names.

--- LambdaZero/datasets/brutal_dock/models.py
+++ LambdaZero/datasets/brutal_dock/models.py
@@ -1,20 +1,21 @@
-import torch as th
 from torch import nn
 from torch.nn import Sequential, Linear, ReLU, GRU, functional as F
 from torch_geometric.nn import NNConv, Set2Set
 
+
 class MessagePassingNet(nn.Module):
     def __init__(self, num_feat=14, dim=64):
         super(MessagePassingNet, self).__init__()
-        self.lin0 = th.nn.Linear(num_feat, dim)
 
-        nn = Sequential(Linear(4, 128), ReLU(), Linear(128, dim * dim))
-        self.conv = NNConv(dim, dim, nn, aggr='mean')
+        self.lin0 = nn.Linear(num_feat, dim)
+
+        mlp = Sequential(Linear(4, 128), ReLU(), Linear(128, dim * dim))
+        self.conv = NNConv(dim, dim, mlp, aggr='mean')
         self.gru = GRU(dim, dim)
 
         self.set2set = Set2Set(dim, processing_steps=3)
-        self.lin1 = th.nn.Linear(2 * dim, dim)
-        self.lin2 = th.nn.Linear(dim, 1)
+        self.lin1 = nn.Linear(2 * dim, dim)
+        self.lin2 = nn.Linear(dim, 1)
 
     def forward(self, data):
         out = F.relu(self.lin0(data.x))
