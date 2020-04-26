20969f4b74c8508979b61e4eba8abae2c7f52e32
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-04-26 14:47:02 -0400

    Fix problematic name for forward function.

--- LambdaZero/datasets/brutal_dock/tests/linear_regression.py
+++ LambdaZero/datasets/brutal_dock/tests/linear_regression.py
@@ -3,8 +3,8 @@ from torch import nn
 
 class LinearRegression(nn.Module):
     def __init__(self, number_of_inputs: int, number_of_outputs: int):
-        super(LinearRegression, self).__init__()
+        super().__init__()
         self.linear = nn.Linear(number_of_inputs, number_of_outputs)
 
-    def normalized_forward(self, x):
+    def forward(self, x):
         return self.linear(x)
