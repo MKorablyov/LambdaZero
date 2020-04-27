9b7dc0148ad3e18e68a5c753731d5601af397b83
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-04-27 09:44:37 -0400

    Use dataset class for better plumbing.

--- LambdaZero/datasets/brutal_dock/experiments/mpnn_experiment.py
+++ LambdaZero/datasets/brutal_dock/experiments/mpnn_experiment.py
@@ -2,21 +2,23 @@
 TODO : description
 """
 import logging
+import shutil
 from pathlib import Path
-import numpy as np
 
+import numpy as np
 import torch
 import torch.nn.functional as F
 from torch_geometric.data import DataLoader
 
 from LambdaZero.datasets.brutal_dock import ROOT_DIR, RESULTS_DIR, BRUTAL_DOCK_DATA_DIR
+from LambdaZero.datasets.brutal_dock.dataset_utils import get_scores_statistics
+from LambdaZero.datasets.brutal_dock.datasets import D4MoleculesDataset
+from LambdaZero.datasets.brutal_dock.experiments import EXPERIMENT_DATA_DIR
 from LambdaZero.datasets.brutal_dock.graph_score_normalizer import GraphScoreNormalizer
 from LambdaZero.datasets.brutal_dock.logger_utils import create_logging_tags
 from LambdaZero.datasets.brutal_dock.mlflow_logger import MLFlowLogger
 from LambdaZero.datasets.brutal_dock.model_trainer import MoleculeModelTrainer
 from LambdaZero.datasets.brutal_dock.models import MessagePassingNet
-from LambdaZero.datasets.brutal_dock.dataset_utils import get_smiles_and_scores_from_feather, \
-    get_molecule_graph_dataset, get_scores_statistics
 from LambdaZero.datasets.model_evaluator import MoleculeModelEvaluator
 
 torch.manual_seed(0)
@@ -26,16 +28,14 @@ path_of_this_file = Path(__file__).resolve()
 experiment_name = 'First MPNN run'
 tracking_uri = str(ROOT_DIR.joinpath("mlruns"))
 
-num_epochs = 100
-dataset_size = 47410
+num_epochs = 10
 batch_size = 128
-
-train_size = int(0.8*dataset_size)
-valid_size = int(0.1*dataset_size)
-test_size = dataset_size - train_size - valid_size
+train_fraction = 0.8
+validation_fraction = 0.1
 
 best_model_path = RESULTS_DIR.joinpath("plumbing_tests")
 d4_feather_data_path = BRUTAL_DOCK_DATA_DIR.joinpath("d4/raw/dock_blocks105_walk40_clust.feather")
+raw_data_path = EXPERIMENT_DATA_DIR.joinpath("/raw/dock_blocks105_walk40_clust.feather")
 
 loss_function = F.mse_loss
 
@@ -47,15 +47,21 @@ mlflow_logger = MLFlowLogger(experiment_name, tracking_uri, logging_tags)
 if __name__ == "__main__":
     logging.info(f"Executing {path_of_this_file}...")
 
-    logging.info(f"Reading SMILES and dockscores from {d4_feather_data_path}")
-    list_smiles, list_scores = get_smiles_and_scores_from_feather(d4_feather_data_path)
+    if not raw_data_path.is_file():
+        logging.info(f"Copying {d4_feather_data_path} to {raw_data_path})")
+        shutil.copy(str(d4_feather_data_path), str(raw_data_path))
 
-    logging.info(f"Converting SMILES to molecular graphs")
-    list_graphs = get_molecule_graph_dataset(list_smiles[:dataset_size], list_scores[:dataset_size])
+    logging.info(f"Creating the full dataset")
+    full_dataset = D4MoleculesDataset(str(EXPERIMENT_DATA_DIR))
+
+    dataset_size = len(full_dataset)
+    train_size = int(train_fraction * dataset_size)
+    valid_size = int(validation_fraction * dataset_size)
+    test_size = dataset_size - train_size - valid_size
 
     logging.info(f"Splitting data into train, validation, test sets")
     training_graphs, validation_graphs, _ = \
-        torch.utils.data.random_split(list_graphs, [train_size, valid_size, test_size])
+        torch.utils.data.random_split(full_dataset, [train_size, valid_size, test_size])
 
     logging.info(f"Extracting mean and standard deviation from training data")
     training_mean, training_std = get_scores_statistics(training_graphs)
