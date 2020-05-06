05e96fdb91efb0ef1fd042bcd8142785b6f21801
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-05-06 15:54:00 -0400

    Fixed dataset test.

--- LambdaZero/datasets/brutal_dock/tests/test_datasets.py
+++ LambdaZero/datasets/brutal_dock/tests/test_datasets.py
@@ -1,4 +1,5 @@
 import logging
+import string
 import tempfile
 from pathlib import Path
 
@@ -11,9 +12,14 @@ from LambdaZero.datasets.brutal_dock.datasets import D4MoleculesDataset
 
 @pytest.fixture
 def fake_smiles_and_gridscore_dataframe():
+    list_smiles = np.array(['CC(C)=CC(C)(C)O', 'CC(C)=CC(=O)NC(C#N)P(=O)(O)O',
+                            'O=[SH](=O)S(=O)(=O)O', 'CC(C)=CN1CCN(P(=O)(O)O)CC1',
+                            'CC(C)(O)C(F)(F)F', 'Nn1cnc2c([PH](=O)[O-])ncnc21',
+                            'c1ccc2cc(N3CCOCC3)ccc2c1', 'CC(C)(O)Br', 'CC(=O)N[SH](=O)=O',
+                            'CC(C)=CC1CC(C(NC(=O)C(C)O)NC(=O)S(=O)(=O)O)N(c2ccc3ccccc3c2)C1C(C)C'])
 
-    list_smiles = ['CC(C)=CC(C)(C)O', 'CC(C)(O)C(F)(F)F', 'O=[SH](=O)S(=O)(=O)O']
-    list_scores = [1., 2., 3.]
+    size = len(list_smiles)
+    list_scores = np.random.random(size)
 
     df = pd.DataFrame(data={'smiles': list_smiles, 'gridscore': list_scores})
 
@@ -26,8 +32,14 @@ def original_raw_data_dir(fake_smiles_and_gridscore_dataframe):
         logging.info("creating a fake directory")
         raw_dir = Path(tmp_dir_str).joinpath("raw")
         raw_dir.mkdir()
-        file_path = raw_dir.joinpath(D4MoleculesDataset.feather_filename)
-        fake_smiles_and_gridscore_dataframe.to_feather(file_path)
+
+        number_of_files = len(D4MoleculesDataset.feather_filenames)
+        list_index_groups = np.array_split(fake_smiles_and_gridscore_dataframe.index, number_of_files)
+
+        for filename, indices in zip(D4MoleculesDataset.feather_filenames, list_index_groups) :
+            file_path = raw_dir.joinpath(filename)
+            sub_df = fake_smiles_and_gridscore_dataframe.iloc[indices].reset_index(drop=True)
+            sub_df.to_feather(file_path)
         yield str(raw_dir)
 
     logging.info("deleting test folder")
@@ -51,4 +63,4 @@ def test_d4_molecules_dataset(root_dir, original_raw_data_dir, fake_smiles_and_g
 
     expected_values = fake_smiles_and_gridscore_dataframe['gridscore'].values
     computed_values = [d.dockscore.item() for d in dataset]
-    np.testing.assert_array_equal(expected_values, computed_values)
+    np.testing.assert_almost_equal(expected_values, computed_values)
