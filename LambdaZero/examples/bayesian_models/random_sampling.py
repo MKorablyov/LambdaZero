f870450ccc8998e247c697b96b3461d04d4639e1
idjoannchen <joannamcat@gmail.com>
2020-05-08 00:24:56 -0400

    implement greedy training

--- LambdaZero/examples/bayesian_models/random_sampling.py
+++ LambdaZero/examples/bayesian_models/random_sampling.py
@@ -3,9 +3,13 @@ import numpy as np
 import torch as th
 from sklearn.model_selection import train_test_split
 import random
+from tqdm import tqdm
 from torch.utils.data import DataLoader
 from matplotlib import pyplot as plt
-import seaborn as sns
+import torch.nn.functional as F
+# import seaborn as sns
+
+device = 'cuda:0' if th.cuda.is_available() else 'cpu'
 
 class cfg:
     # data could be found here: https://github.com/MKorablyov/brutal_dock/tree/master/d4/raw
@@ -22,6 +26,121 @@ class cfg:
 # S1: greedy
 # batch_size = 500, num_interations = 6
 
+# Notes: I didn't use the random_split function b/c it needs data_loader function.
+def random_split(dataset, test_prob, valid_prob,
+                  test_idx=th.tensor([], dtype=th.long),
+                  train_idx=th.tensor([], dtype=th.long),
+                  val_idx=th.tensor([], dtype=th.long)):
+    # todo assert order
+    num_last_split = (len(test_idx) + len(train_idx) + len(val_idx))
+    num_split = len(dataset) - num_last_split
+
+    ntest = int(num_split * test_prob)
+    nvalid = int(num_split * valid_prob)
+    idx = th.randperm(num_split) + num_last_split
+
+    test_idx = th.cat([test_idx, idx[:ntest]])
+    val_idx = th.cat([val_idx, idx[ntest:ntest + nvalid]])
+    train_idx = th.cat([train_idx, idx[ntest + nvalid:]])
+
+    return test_idx, val_idx, train_idx
+
+def train_epoch(x, y, model, optimizer):
+    model.train()
+    batch_size = cfg.batch_size
+    loss_all = 0
+    losses = []
+    for i in tqdm(range(0, len(x), batch_size)):
+        x_in = x[i:i+batch_size]
+        x_in = x_in.to(device)
+        y_in = y[i:i+batch_size]
+        y_in = y_in.to(device)
+        optimizer.zero_grad()
+        preds = model(x_in)
+        loss = F.mse_loss(preds.squeeze(1), y_in)
+        loss.backward()
+        optimizer.step()
+        loss_all += loss.item()
+        losses.append(loss)
+    return loss_all / len(x), loss
+
+def train_greedy(x, y, model, optimizer):
+    model.train()
+    batch_size = cfg.batch_size
+    loss_all = 0
+    losses = []
+    for i in tqdm(range(0, len(x), batch_size)):
+        x_in = x[i:i+batch_size]
+        x_in = x_in.to(device)
+        y_in = y[i:i+batch_size]
+        y_in = y_in.to(device)
+        optimizer.zero_grad()
+        preds = model(x_in)
+        loss = F.mse_loss(preds.squeeze(1), y_in)
+        loss.backward()
+        optimizer.step()
+        loss_all += loss.item()
+        losses.append(loss)
+    return loss_all / len(x), loss
+
+def train_greedy_error(x, y, model, optimizer):
+    model.train()
+    batch_size = cfg.batch_size
+    loss_all = 0
+    losses = []
+    bound = 0
+
+    for i in tqdm(range(0, len(x), batch_size)):
+        if bound == 0:
+            bound = i + batch_size
+        x_in = x[0:bound]
+        x_in = x_in.to(device)
+        y_in = y[0:bound]
+        y_in = y_in.to(device)
+
+        x_rest = x[bound:]
+        x_rest = x_in.to(device)
+        y_rest = y[bound:]
+        y_rest = y_in.to(device)
+
+        for _ in range(cfg.epochs):
+            optimizer.zero_grad()
+            preds = model(x_in)
+            loss = F.mse_loss(preds.squeeze(1), y_in)
+            loss.backward()
+            optimizer.step()
+        preds = model(x_rest)
+        print(preds.shape)
+        
+        tmp = np.concatenate((x_rest.detach().cpu().numpy(), preds.detach().cpu().numpy()), axis = 2)
+        tmp = np.sort(tmp, axis = 2)
+        print(tmp.shape)
+        bests = preds[:i * batch_size]
+        loss_all += loss.item()
+        losses.append(loss)
+    return loss_all / len(x), loss
+
+def test_epoch(x, y, model):
+    model.eval()
+    ys = []
+    preds = []
+    batch_size = cfg.batch_size
+    for i in tqdm(range(0, len(x), batch_size)):
+        x_in = x[i:i+batch_size]
+        x_in = x_in.to(device)
+        y_in = y[i:i+batch_size]
+        y_in = y_in.to(device)
+        ys.append(y_in.detach().cpu().numpy())
+        preds.append(model(x_in).detach().cpu().numpy())
+
+    ys = np.concatenate(ys, axis=0)
+    preds = np.concatenate(preds, axis=0)
+    mse = np.mean((ys - preds) ** 2)
+    mae = np.mean(np.abs(ys - preds))
+    #test_out = pd.DataFrame({"ys": ys, "preds": preds})
+    return mse, mae, ys, preds
+
+
 class Model(th.nn.Module):
     def __init__(self, inp_size, output_size):
         super(Model, self).__init__()
@@ -34,61 +153,57 @@ class Model(th.nn.Module):
         rel = self.relu(fc)
         return self.fc2(rel)
 
-def train(model, states, labels):
-    batch_size = cfg.batch_size
-    net  = model
-    
-    loss_list = []
-    epochs = cfg.epochs
-    criterion =  th.nn.MSELoss()
-    criterion2 =  th.nn.L1Loss()
-    optimizer = th.optim.SGD(net.parameters(), lr = 0.001)
-
-    for _ in range(epochs):
-        for j in range(0, len(states), batch_size):
-            x = states[j:j+batch_size]
-            y = labels[j:j + batch_size] 
-            optimizer.zero_grad()
-            outputs = net(x)
-            loss = criterion(outputs, th.tensor([y], dtype = th.float32))
-            loss2 = criterion2(outputs, th.tensor([y], dtype = th.float32))
-            loss.backward()
-            optimizer.step()     
-
-    return model, loss, loss2
-
-def evaluate(model, states, labels):
-    criterion = th.nn.MSELoss()
-    criterion2 = th.nn.L1Loss()
-
-    predictions = model(states)
-    MSE = criterion(predictions, th.tensor([labels], dtype = th.float32))
-    MAE = criterion2(predictions, th.tensor([labels], dtype = th.float32))
-
-    return predictions, MSE, MAE
-
 if __name__ == '__main__':
     sample_size = [10,100, 1000, 10000,100000]#9558
+
     test_mae = []
     train_mae = []
     df1 = pd.read_parquet(cfg.data)
-    for i in sample_size:
-        df = df1.sample(n = i)
-        x = th.from_numpy(np.vstack(df['fingerprint'].values).astype(np.float32))
-        y = list(df['dockscore'].values)
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
-        
-        model = Model(x.shape[0], 1)
-        model, MSE, MAE = train(model, x_train, y_train)
-        train_mae.append(MAE)
-        print('running MAE after training: {}'.format(MAE.item()))
-        predictions, MSE, MAE = evaluate(model, x_test, y_test)
-        test_mae.append(MAE)
-        print('MAE after eval: {}'.format(MAE.item()))
+    #df = df1.sample()
+    x = th.from_numpy(np.vstack(df1['fingerprint'].values).astype(np.float32))
+    y = th.Tensor(list(df1['dockscore'].values))
+    model = Model(1024, 1)#(x.shape[0], 1)
 
-    plt.plot(sample_size, test_mae, color="r", linestyle="-", marker="^", linewidth=1,label="Test")
-    plt.plot(sample_size, train_mae, color="b", linestyle="-", marker="s", linewidth=1,label="Train")
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
+    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size = 0.2)
+
+    optimizer = th.optim.Adam(model.parameters())
+
+    train_errors = []
+    test_errors = []
+    val_errors = []
+
+    for i in range(cfg.epochs):
+        
+        tmp = list(zip(x_train, y_train))
+        random.shuffle(list(tmp))
+        x_train, y_train = zip(*tmp)
+        x_train = np.asarray([t.numpy() for t in x_train], dtype = np.float32)
+        y_train = np.asarray([t.numpy() for t in y_train], dtype = np.float32)
+        x_train = th.FloatTensor(x_train)
+        y_train = th.FloatTensor(y_train)
+
+        avg_loss, train_mse = train_epoch(x_train, y_train, model, optimizer)
+        mse, mae, ys, preds = test_epoch(x_test, y_test, model)
+
+        train_errors.append(train_mse)
+        test_errors.append(mse)
+    
+        mse, mae, ys, preds = test_epoch(x_val, y_val, model)
+        val_errors.append(mse)
+
+    plt.plot(np.linspace(0, len(x_test), 6), test_errors, color="r", linestyle="-", marker="^", linewidth=1,label="Test")
     plt.legend(loc='upper left', bbox_to_anchor=(0.2, 0.95))
     plt.xlabel("Sample Size")
-    plt.ylabel("MAE")
+    plt.ylabel("MSE")
+    plt.show()
+    plt.plot(np.linspace(0, len(x_train), 6), train_errors, color="b", linestyle="-", marker="s", linewidth=1,label="Train")
+    plt.legend(loc='upper left', bbox_to_anchor=(0.2, 0.95))
+    plt.xlabel("Sample Size")
+    plt.ylabel("MSE")
+    plt.show()
+    plt.plot(np.linspace(0, len(x_val), 6), val_errors, color="g", linestyle="-", marker="c", linewidth=1,label="Val")
+    plt.legend(loc='upper left', bbox_to_anchor=(0.2, 0.95))
+    plt.xlabel("Sample Size")
+    plt.ylabel("MSE")
     plt.show()
