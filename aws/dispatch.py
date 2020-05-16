8ac2507fb874bdf6d43371d2a60001f294979736
Arnaud Bergeron <abergeron@gmail.com>
2020-05-16 13:14:56 -0400

    Use a real temp dir for home

--- aws/dispatch.py
+++ aws/dispatch.py
@@ -147,10 +147,7 @@ def find_next_batch(init_i=0, init_j=0):
 
 @ray.remote(num_cpus=1)
 def do_docking(i, j, k, results_dir):
-    real_home = os.getenv('REAL_HOME', os.getenv('HOME'))
-    os.environ['REAL_HOME'] = real_home
-    os.environ['HOME'] = real_home + "/homes/{0}/{1}/{2}/".format(i, j, k)
-    os.makedirs(os.environ['HOME'], exist_ok=True)
+    os.environ['HOME'] = tempfile.mkdtemp()
     workpath = "/tmp/docking/{0}/{1}/{2}/".format(i, j, k)
     os.makedirs(workpath, exist_ok=True)
     dock_smi = Dock_smi(outpath=workpath,
