95b8df61bb45732edd787584f627cfdd1f219134
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-05-22 09:41:29 -0400

    Import missing pytest.

--- LambdaZero/datasets/brutal_dock/tests/test_hierarchical_mpnn.py
+++ LambdaZero/datasets/brutal_dock/tests/test_hierarchical_mpnn.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+
+import pytest
+
 from LambdaZero.datasets.brutal_dock.models.model_base import get_list_of_smiles_from_batch
 import LambdaZero.datasets.brutal_dock.models.hierarchical_mpnn as hmpnn
 
