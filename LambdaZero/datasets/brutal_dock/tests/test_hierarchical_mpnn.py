ab34e62d04dc51a597355d6c63a7b993ef96128a
Simon Verret <verret.simon@gmail.com>
2020-05-21 22:47:26 -0400

    forgotten line in the tests

--- LambdaZero/datasets/brutal_dock/tests/test_hierarchical_mpnn.py
+++ LambdaZero/datasets/brutal_dock/tests/test_hierarchical_mpnn.py
@@ -1,3 +1,4 @@
+import pytest
 from pathlib import Path
 from LambdaZero.datasets.brutal_dock.models.model_base import get_list_of_smiles_from_batch
 import LambdaZero.datasets.brutal_dock.models.hierarchical_mpnn as hmpnn
