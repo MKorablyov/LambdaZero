a1a5ed7d415cfaa8a3ad275ea41f8a3df07dc4eb
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-05-27 19:47:56 -0400

    Smoke test for the optimized chemprop model.

--- LambdaZero/datasets/brutal_dock/tests/test_models.py
+++ LambdaZero/datasets/brutal_dock/tests/test_models.py
@@ -1,8 +1,23 @@
 import pytest
 import torch
+from chemprop.features import BatchMolGraph
 
-from LambdaZero.datasets.brutal_dock.models.chemprop_model import ChempropNet
+from LambdaZero.datasets.brutal_dock.chemprop_adaptors.utils import get_chemprop_graphs_from_raw_data_dataframe
+from LambdaZero.datasets.brutal_dock.models.chemprop_model import ChempropNet, OptimizedChempropNet
 from LambdaZero.datasets.brutal_dock.models.message_passing_model import MessagePassingNet
+
+
+@pytest.fixture
+def chemprop_parameters():
+    parameters = {'name': 'chemprop', 'depth': 2, 'ffn_num_layers': 2, 'ffn_hidden_size': 8}
+    return parameters
+
+
+@pytest.fixture
+def batch_mol_graph(smiles_and_scores_dataframe):
+    list_data_dict = get_chemprop_graphs_from_raw_data_dataframe(smiles_and_scores_dataframe)
+    return BatchMolGraph([d['mol_graph'] for d in list_data_dict])
+
 
 
 @pytest.fixture
@@ -29,20 +44,25 @@ def test_message_passing_net(mpnn_model, random_molecule_batch):
     _ = mpnn_model.forward(random_molecule_batch)
 
 
-def test_chemprop_net(real_molecule_batch):
+def test_chemprop_net(real_molecule_batch, chemprop_parameters):
     """
     A smoke test showing that the chemprop model runs on data of the expected shape.
     """
-
-    parameters = {'name': 'chemprop', 'depth': 2, 'ffn_num_layers': 2, 'ffn_hidden_size': 8}
-
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     batch = real_molecule_batch.to(device)
 
-    net = ChempropNet(**parameters)
+    net = ChempropNet(**chemprop_parameters)
     net.to(device)
 
     _ = net.forward(batch)
 
 
+def test_optimized_chemprop_net(batch_mol_graph, chemprop_parameters):
+    """
+    A smoke test showing that the chemprop model runs on data of the expected shape.
+    """
+
+    net = OptimizedChempropNet(**chemprop_parameters)
+
+    _ = net.forward(batch_mol_graph)
