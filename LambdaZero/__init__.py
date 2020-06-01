c4ee6766060676f70b3b1cd3b5350e5c76031ab2
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-06-01 10:18:54 -0400

    Define global path variables in the top __init__ file.

---
+++ LambdaZero/__init__.py
@@ -0,0 +1,26 @@
+import logging.config
+from pathlib import Path
+
+import yaml
+
+# Define the path to the project's root directory.
+ROOT_DIR = Path(__file__).parent.parent
+ROOT_DIR.mkdir(parents=True, exist_ok=True)
+
+LOG_DIR = ROOT_DIR.joinpath("logs/")
+LOG_DIR.mkdir(parents=True, exist_ok=True)
+
+
+def set_logging_directory(logging_directory: Path):
+    logging_path = ROOT_DIR.joinpath("logging.yaml")
+    with open(logging_path, "rt") as f:
+        config = yaml.safe_load(f.read())
+
+    for log_type in ["info", "error"]:
+        config["handlers"][f"{log_type}_file_handler"]["filename"] = str(
+            logging_directory.joinpath(f"{log_type}.log")
+        )
+    logging.config.dictConfig(config)
+
+
+set_logging_directory(LOG_DIR)
