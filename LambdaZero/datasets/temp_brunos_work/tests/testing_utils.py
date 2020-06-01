11192adea4b4adc3b50a629bc8b8a17a897f415f
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-06-01 09:46:09 -0400

    removed duplicated tests, which are present in LambdaZero/tests

--- LambdaZero/datasets/temp_brunos_work/tests/testing_utils.py
+++
@@ -1,9 +0,0 @@
-import string
-
-import numpy as np
-
-
-def generate_random_string(string_length):
-    letters = [letter for letter in string.ascii_uppercase]
-    s = np.random.choice(letters, string_length)
-    return ''.join(s)
