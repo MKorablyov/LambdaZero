3c8f2f51d755274b2c8524e868a1967c193de6c0
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-06-08 14:13:11 -0400

    Better names; better comments.

--- chimera_install/download_chimera_dmg.py
+++ chimera_install/download_chimera_dmg.py
@@ -2,9 +2,11 @@
 """
 This script downloads the chimera dmg from the UCSF website. We need to use
 a somewhat sophisticated approach here because the website requires to "accept" some
-conditions and then there's more hoops to go through.
+conditions and then there's a redirect on the download page. This redirect contains an actual download
+link which seems to contain a token which is only valid for a finite amount of time.
 
-wget on the remote_url below doesn't seem to work. Using requests works.
+Simply using wget on the download url does not work, and the download url eventually expires. Thus,
+we have to go through the accept page to get a download url with a valid token, and then download the actual dmg.
 """
 from pathlib import Path
 import re
@@ -12,19 +14,21 @@ import re
 import requests
 
 
-def extract_download_sub_url(redirect_page_html: str) -> str:
+def extract_download_relative_url(redirect_page_html: str) -> str:
     """
     We use regex to parse the html in order to avoid
-    introducing a dependency on beautiful soup or some other
-    html parsing library.
+    introducing a dependency on an html parsing library.
+
+    The left and right anchor below we obtained by inspection of the redirect page html.
+
     """
-    left_achor = '<a href="'
-    right_achor = '">'
-    pattern = f'{left_achor}.*{right_achor}'
+    left_anchor = '<a href="'
+    right_anchor = '">'
+    pattern = f'{left_anchor}.*{right_anchor}'
     matched_string = re.search(pattern, redirect_page_html).group()
 
-    sub_url = matched_string.replace(left_achor, "").replace(right_achor, "")
-    return sub_url
+    relative_url = matched_string.replace(left_anchor, "").replace(right_anchor, "")
+    return relative_url
 
 
 def download_file(remote_url: str, local_file_path: str):
@@ -36,12 +40,8 @@ def download_file(remote_url: str, local_file_path: str):
 
 
 base_url = "https://www.cgl.ucsf.edu"
-chimera_conditions_page_url = f"{base_url}/chimera/cgi-bin/secure/chimera-get.py?file=mac64/chimera-1.14-mac64.dmg"
-
-# This remote url was obtained by inspection, playing around with the download page
-remote_url = "https://www.cgl.ucsf.edu/chimera/cgi-bin/secure/chimera-get.py?ident=OHeQer2WTadn%2F%2BVltnhB5%2BBgukNERNvx1hxz3g3ghO4rrg%3D%3D&file=mac64%2Fchimera-1.14-mac64.dmg&choice=Notified"
-
 filename = "chimera-1.14-mac64.dmg"
+chimera_conditions_page_url = f"{base_url}/chimera/cgi-bin/secure/chimera-get.py?file=mac64/{filename}"
 
 if __name__ == '__main__':
 
@@ -53,12 +53,12 @@ if __name__ == '__main__':
     redirect_page_html = r.content.decode('utf-8')
 
     # extract the redirected url for download
-    download_sub_url = extract_download_sub_url(redirect_page_html)
-    download_url = f"{base_url}/{download_sub_url}"
+    download_relative_url = extract_download_relative_url(redirect_page_html)
+    download_url = f"{base_url}/{download_relative_url}"
 
     dir = Path(__file__).parent
     local_file_path = str(dir.joinpath(filename))
-    print(f"Downoading file {filename} from {remote_url}...")
+    print(f"Downoading file {filename} from {download_url}...")
     download_file(download_url, local_file_path)
     print(f"Finished downloading.")
 
