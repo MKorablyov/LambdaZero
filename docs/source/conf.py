081aa613f24c1bba1e28c89ef7572dc76387900d
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-06-10 16:36:47 -0400

    remove needless template and _static folder dependencies

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ release = 'v0.0.1'
 extensions = ['sphinx.ext.napoleon',  "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = []
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -53,7 +53,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 source_parsers = {'.md': 'recommonmark.parser.CommonMarkParser', }
 source_suffix = ['.rst', '.md']
