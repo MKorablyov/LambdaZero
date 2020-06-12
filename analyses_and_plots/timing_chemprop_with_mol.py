2677fa0e8f597e5ef7bf8f0d6f2c22fa7e297585
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-06-12 10:26:54 -0400

    Fixed imports in timing analysis experiments.

--- analyses_and_plots/timing_chemprop_with_mol.py
+++ analyses_and_plots/timing_chemprop_with_mol.py
@@ -2,16 +2,16 @@
 The goal of this analysis script is to identify the execution bottleneck in the chemprop model so
 we can focus on the correct thing to improve to accelerate execution.
 """
-from LambdaZero.representation_learning.chemprop_adaptors.dataloader_utils import get_chemprop_dataloaders
 from LambdaZero.loggers.null_logger import NullLogger
+from LambdaZero.oracle_models.chemprop_model import MolGraphChempropNet
+from LambdaZero.representation_learning.chemprop_adaptors.dataloader_utils import get_chemprop_dataloaders
 from LambdaZero.representation_learning.chemprop_adaptors.model_trainer import ChempropModelTrainer
 from LambdaZero.representation_learning.datasets import D4ChempropMoleculesDataset
 from LambdaZero.representation_learning.experiment_driver import experiment_driver
-from LambdaZero.representation_learning.models.chemprop_model import OptimizedChempropNet
 from analyses_and_plots.chemprop_parameters import input_and_run_config
 
 dataset_class = D4ChempropMoleculesDataset
-model_class = OptimizedChempropNet
+model_class = MolGraphChempropNet
 
 if __name__ == '__main__':
 
--- analyses_and_plots/timing_chemprop_with_smiles.py
+++ analyses_and_plots/timing_chemprop_with_smiles.py
@@ -6,11 +6,11 @@ we can focus on the correct thing to improve to accelerate execution.
 from LambdaZero.loggers.null_logger import NullLogger
 from LambdaZero.representation_learning.datasets import D4GeometricMoleculesDataset
 from LambdaZero.representation_learning.experiment_driver import experiment_driver
-from LambdaZero.representation_learning.models.chemprop_model import ChempropNet
+from LambdaZero.oracle_models.chemprop_model import GeometricChempropNet
 from analyses_and_plots.chemprop_parameters import input_and_run_config
 
 dataset_class = D4GeometricMoleculesDataset
-model_class = ChempropNet
+model_class = GeometricChempropNet
 
 if __name__ == '__main__':
 
