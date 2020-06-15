1934e474915e1f798306b15415cc494ce78c1407
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-06-15 16:19:24 -0400

    Simplified the dataset to not move files around.

--- LambdaZero/examples/chemprop/datasets.py
+++ LambdaZero/examples/chemprop/datasets.py
@@ -1,7 +1,5 @@
 import logging
-import shutil
 from pathlib import Path
-from typing import List
 
 import pandas as pd
 from torch.utils.data import Dataset
@@ -12,26 +10,10 @@ from LambdaZero.chemprop_adaptors.utils import get_chemprop_graphs_from_raw_data
 class D4ChempropMoleculesDataset(Dataset):
     raw_file_names = ["dock_blocks105_walk40_clust.feather", "dock_blocks105_walk40_2_clust.feather"]
 
-    def __init__(self, root_dir: str, original_raw_data_dir: str):
-        self.original_raw_data_dir_path = Path(original_raw_data_dir)
-        self.root_dir_path = Path(root_dir)
-
-        self._move_files(list_raw_file_names=self.raw_file_names,
-                         destination_directory=root_dir)
+    def __init__(self, raw_data_dir: str):
+        self.raw_data_dir_path = Path(raw_data_dir)
 
         self._list_data = self._process()
-
-    def _move_files(self, list_raw_file_names: List[str], destination_directory: str):
-
-        destination_directory_path = Path(destination_directory)
-
-        for raw_file_name in list_raw_file_names:
-            destination_path = destination_directory_path.joinpath(raw_file_name)
-            origin_path = self.original_raw_data_dir_path.joinpath(raw_file_name)
-
-            if not destination_path.is_file():
-                logging.info(f"Copying {origin_path} to {destination_path})")
-                shutil.copy(str(origin_path), str(destination_path))
 
     def _process(self):
         # Read data into huge `Data` list.
@@ -39,7 +21,7 @@ class D4ChempropMoleculesDataset(Dataset):
 
         list_df = []
         for raw_file_name in self.raw_file_names:
-            feather_data_path = self.root_dir_path.joinpath(raw_file_name)
+            feather_data_path = self.raw_data_dir_path.joinpath(raw_file_name)
             df = pd.read_feather(feather_data_path)
             list_df.append(df)
 
