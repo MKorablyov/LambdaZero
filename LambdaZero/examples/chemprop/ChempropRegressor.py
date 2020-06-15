b1a76a6b47e4a038a8751c21793084530c9b8316
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-06-15 16:18:54 -0400

    Full regressor implementation and smoke test.

--- LambdaZero/examples/chemprop/ChempropRegressor.py
+++ LambdaZero/examples/chemprop/ChempropRegressor.py
@@ -1,82 +1,124 @@
+from typing import Dict
+
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 import sys
 
 from LambdaZero.chemprop_adaptors.dataloader_utils import chemprop_collate_fn
+from LambdaZero.datasets.dataset_splitting import RandomDatasetSplitter
+from LambdaZero.examples.chemprop.datasets import D4ChempropMoleculesDataset
 from LambdaZero.oracle_models.chemprop_model import MolGraphChempropNet
 from LambdaZero.trainable.base_pytorch_regressor import BasePytorchRegressor
 import torch
+from torch.nn.functional import mse_loss
 
 
 class ChempropRegressor(BasePytorchRegressor):
-    loss_function = torch.nn.functional.mse_loss
-
     def get_model(self, config):
         return MolGraphChempropNet(**config["model_parameters"])
 
     @staticmethod
     def _normalize_target(y, mean, std):
-        return (y - mean)/std
+        return (y - mean) / std
 
     @staticmethod
     def _denormalize_target(normalized_y, mean, std):
-        return std*normalized_y + mean
+        return std * normalized_y + mean
 
-    def _get_batch_loss(self, batch, model, config):
-        mean = torch.tensor(config["target_norm"][0], device=self.device, requires_grad=False)
-        std = torch.tensor(config["target_norm"][1], device=self.device, requires_grad=False)
+    def _get_size_of_batch(self, batch: Dict, config):
+        return len(batch[config["target"]])
 
-        batch = batch.to(self.device)
-        y_actual = batch[config["target"]]
+    def _get_batch_loss(self, batch: Dict, model, config):
+        mean = torch.tensor(
+            config["target_norm"][0], device=self.device, requires_grad=False
+        )
+        std = torch.tensor(
+            config["target_norm"][1], device=self.device, requires_grad=False
+        )
+
+        batch_mol_graph = batch["mol_graph"]
+        y_actual = batch[config["target"]].to(self.device)
 
         normalized_y_actual = self._normalize_target(y_actual, mean, std)
 
         model.to(self.device)
-        normalized_y_predicted = model.forward(batch)
+        normalized_y_predicted = model.forward(batch_mol_graph)
 
-        batch_loss = self.loss_function(normalized_y_actual, normalized_y_predicted)
+        batch_loss = mse_loss(normalized_y_actual, normalized_y_predicted)
         return batch_loss
 
     def train_epoch(self, training_dataloader, model, optim, device, config):
         model.train()
         total_epoch_loss = 0.0
 
-        number_of_batches = len(training_dataloader)
-
         for batch in tqdm(training_dataloader, desc="TRAIN", file=sys.stdout):
             self.optim.zero_grad()
-            batch_loss = self._get_batch_loss(batch, model)
+            batch_loss = self._get_batch_loss(batch, model, config)
             batch_loss.backward()
             self.optim.step()
 
             batch_loss_value = batch_loss.item()
 
-            total_epoch_loss += batch_loss_value*self._get_size_of_batch(batch)
+            total_epoch_loss += batch_loss_value * self._get_size_of_batch(
+                batch, config
+            )
 
-        average_epoch_loss = total_epoch_loss/len(dataloader.dataset)
-        return average_epoch_loss
+        average_epoch_loss = total_epoch_loss / len(training_dataloader.dataset)
+        return {"mse": average_epoch_loss}
 
-    def eval_epoch(self, validation_set, model, device, config):
-        pass
+    def eval_epoch(self, validation_dataloader, model, device, config):
+        model.eval()
+        total_epoch_loss = 0.0
+
+        for batch in tqdm(validation_dataloader, desc="VALID", file=sys.stdout):
+            batch_loss = self._get_batch_loss(batch, model, config)
+            batch_loss_value = batch_loss.item()
+            total_epoch_loss += batch_loss_value * self._get_size_of_batch(
+                batch, config
+            )
+
+        average_epoch_loss = total_epoch_loss / len(validation_dataloader.dataset)
+
+        return {"mse": average_epoch_loss}
+
+    def get_dataset(self, config):
+        dataset = D4ChempropMoleculesDataset(config["dataset_root"])
+        return dataset
 
     def get_dataloaders(self, config):
+        dataset = self.get_dataset(config)
+
+        splitter = RandomDatasetSplitter(
+            train_fraction=0.8,
+            validation_fraction=0.1,
+            random_seed=config["random_seed"],
+        )
+
+        training_dataset, validation_dataset, test_dataset = splitter.get_split_datasets(
+            dataset
+        )
 
         batch_size = config["b_size"]
 
-        training_dataloader = DataLoader(training_dataset,
-                                         batch_size=batch_size,
-                                         collate_fn=chemprop_collate_fn,
-                                         shuffle=True)
+        training_dataloader = DataLoader(
+            training_dataset,
+            batch_size=batch_size,
+            collate_fn=chemprop_collate_fn,
+            shuffle=True,
+        )
 
-        validation_dataloader = DataLoader(validation_dataset,
-                                           batch_size=batch_size,
-                                           collate_fn=chemprop_collate_fn,
-                                           shuffle=True)
+        validation_dataloader = DataLoader(
+            validation_dataset,
+            batch_size=batch_size,
+            collate_fn=chemprop_collate_fn,
+            shuffle=True,
+        )
 
-        test_dataloader = DataLoader(test_dataset,
-                                     batch_size=batch_size,
-                                     collate_fn=chemprop_collate_fn,
-                                     shuffle=False)
+        test_dataloader = DataLoader(
+            test_dataset,
+            batch_size=batch_size,
+            collate_fn=chemprop_collate_fn,
+            shuffle=False,
+        )
 
         return training_dataloader, validation_dataloader, test_dataloader
-
---
+++ tests/test_chemprop_regressor.py
@@ -0,0 +1,96 @@
+import logging
+import numpy as np
+import tempfile
+from pathlib import Path
+
+import pytest
+import ray
+from ray.tune import tune
+
+from LambdaZero.examples.chemprop.ChempropRegressor import ChempropRegressor
+
+
+@pytest.fixture
+def expected_raw_files():
+    return [
+        "dock_blocks105_walk40_clust.feather",
+        "dock_blocks105_walk40_2_clust.feather",
+    ]
+
+@pytest.fixture
+def data_dir(expected_raw_files, smiles_and_scores_dataframe):
+
+    with tempfile.TemporaryDirectory() as tmp_dir_str:
+        logging.info("creating a fake directory")
+        data_dir_path = Path(tmp_dir_str)
+
+        logging.info("creating fake raw data files")
+        index_splits = np.array_split(
+            smiles_and_scores_dataframe.index, len(expected_raw_files)
+        )
+        for filename, indices in zip(expected_raw_files, index_splits):
+            sub_df = smiles_and_scores_dataframe.loc[indices].reset_index(drop=True)
+            file_path = data_dir_path.joinpath(filename)
+            sub_df.to_feather(file_path)
+        yield data_dir_path
+    logging.info("deleting test folder")
+
+
+@pytest.fixture
+def summaries_dir():
+    with tempfile.TemporaryDirectory() as tmp_dir_str:
+        logging.info("creating a fake directory")
+        yield Path(tmp_dir_str)
+    logging.info("deleting test folder")
+
+@pytest.fixture
+def model_parameters():
+
+    model_parameters = {
+        "name": "chemprop",
+        "bias": False,
+        "hidden_size": 5,
+        "depth": 2,
+        "dropout": 0.0,
+        "atom_messages": False,
+        "undirected": False,
+        "ffn_hidden_size": 7,
+        "ffn_num_layers": 3}
+
+    return model_parameters
+
+
+@pytest.fixture
+def config(model_parameters, data_dir, summaries_dir):
+
+    config = {"dataset_root": str(data_dir),
+              "random_seed": 0,
+              "target": "gridscore",
+              "target_norm": [-26.3, 12.3],
+              "lr": 0.001,
+              "b_size": 64,
+              "num_epochs": 1,
+              "model_parameters": model_parameters,
+              "summaries_dir": summaries_dir,
+              }
+
+    return config
+
+
+def test_chemprop_regressor(config):
+    tunable_regressor = ChempropRegressor(config=config)
+
+
+def test_smoke_test_tuning_chemprop_regressor(config):
+    """
+    This is a SMOKE TEST. It just validates that the code will run without errors if given
+    expected inputs. It does not validate that the results are correct.
+    """
+    ray.init(local_mode=True)
+
+    _ = tune.run(ChempropRegressor,
+                 config=config,
+                 stop={"training_iteration": 2},
+                 resources_per_trial={"cpu": 1, "gpu": 0.0},
+                 num_samples=1,
+                 local_dir=config["summaries_dir"])
