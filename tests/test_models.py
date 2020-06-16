c6e00305a9606ba1764b313b572eb9f960cdedb8
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-06-16 12:51:31 -0400

    Removed test of chemprop model on smiles.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,16 +1,15 @@
+import itertools
 from contextlib import contextmanager
 
 import pytest
 import torch
 from chemprop.features import BatchMolGraph
-import itertools
 
-from LambdaZero.oracle_models.chemprop_model import (
-    GeometricChempropNet,
-    MolGraphChempropNet,
-)
 from LambdaZero.examples.chemprop.utils import (
     get_chemprop_graphs_from_raw_data_dataframe,
+)
+from LambdaZero.oracle_models.chemprop_model import (
+    MolGraphChempropNet,
 )
 from LambdaZero.oracle_models.message_passing_model import (
     MessagePassingNet,
@@ -126,28 +125,6 @@ def input_parameter_context(chemprop_parameters):
         return pytest.raises(NotImplementedError)
     else:
         return does_not_raise()
-
-
-@pytest.mark.parametrize(
-    "bias, hidden_size, depth, dropout, atom_messages, undirected, ffn_hidden_size, ffn_num_layers",
-    all_chemprop_parameters,
-)
-def test_chemprop_net(
-    real_molecule_batch, chemprop_parameters, input_parameter_context
-):
-    """
-    A smoke test showing that the chemprop model runs on data of the expected shape.
-    """
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    batch = real_molecule_batch.to(device)
-
-    with input_parameter_context:
-        # chemprop is a bit nasty and half puts itself on cuda on its own. Let's make it right.
-        net = GeometricChempropNet(**chemprop_parameters)
-        net.to(device)
-
-        _ = net.forward(batch)
 
 
 @pytest.mark.parametrize(
