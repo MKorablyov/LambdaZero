20ae92e3a3f9d6675f74ed4f88041524625acdf8
Emmanuel Bengio <bengioe@gmail.com>
2020-06-25 14:03:39 -0400

    merge cleanup

--- LambdaZero/environments/__init__.py
+++ LambdaZero/environments/__init__.py
@@ -4,8 +4,4 @@ from .block_mol_v4 import BlockMolEnv_v4
 from .block_mol_v5 import BlockMolEnv_v5
 from .block_mol_graph_v1 import BlockMolEnvGraph_v1
 from .molMDP import MolMDP
-<<<<<<< HEAD
-from .reward import PredDockReward, PredDockReward_v2, QEDReward, MorganDistReward
-=======
 from .reward import PredDockReward, PredDockReward_v2, PredDockReward_v3, QEDReward, MorganDistReward
->>>>>>> master
