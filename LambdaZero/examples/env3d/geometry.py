4df0250ce346503f3d3687c2e7d89004edd462c2
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-07-07 08:34:53 -0400

    Put an assert; defensive programming.

--- LambdaZero/examples/env3d/geometry.py
+++ LambdaZero/examples/env3d/geometry.py
@@ -2,4 +2,5 @@ import numpy as np
 
 
 def get_geometric_center(list_positions: np.array) -> np.array:
+    assert list_positions.shape[1] == 3, "error: the rows are expected to be 3D vectors."
     return list_positions.mean(axis=0)
