636b6026ae75a6c96ab9fb96439cf3798661c9aa
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-07-09 14:14:25 -0400

    position, force field and energy utilities.

--- LambdaZero/examples/env3d/rdkit_utilities.py
+++ LambdaZero/examples/env3d/rdkit_utilities.py
@@ -2,6 +2,7 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.Chem.rdchem import Mol
 import numpy as np
+from rdkit.Geometry.rdGeometry import Point3D
 
 
 def get_atomic_masses(mol: Mol):
@@ -15,5 +16,27 @@ def optimize_mol_in_place(mol: Mol):
 
     Chem.AddHs(mol)
     AllChem.EmbedMolecule(mol)
-    AllChem.MMFFOptimizeMolecule(mol)
+    AllChem.MMFFOptimizeMolecule(mol, mmffVariant='MMFF94')
     Chem.RemoveHs(mol)
+
+
+def set_positions_on_conformer(mol: Mol, positions: np.array):
+    for i, position in enumerate(positions):
+        pt = Point3D()
+        pt.x, pt.y, pt.z = position
+        mol.GetConformer().SetAtomPosition(i, pt)
+
+
+def get_mmff_force_field(mol: Mol, confId: int=-1):
+    """
+    This hack is necessary to pass the correct information to the force field constructor
+    """
+    properties = AllChem.MMFFGetMoleculeProperties(mol, mmffVariant='MMFF94')
+
+    return AllChem.MMFFGetMoleculeForceField(mol, properties, confId)
+
+
+def get_mmff_energy(mol_with_hydrogens: Mol):
+    properties = AllChem.MMFFGetMoleculeProperties(mol_with_hydrogens, mmffVariant='MMFF94')
+    energy = AllChem.MMFFGetMoleculeForceField(mol_with_hydrogens, properties).CalcEnergy()
+    return energy
