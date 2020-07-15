d31cdaf56aabe656384a00c01c481d506d038b98
MKorablyov <MKorablyov@gmail.com>
2020-07-14 21:22:32 -0400

    Update beluga_load_env.sh

--- misc/beluga_load_env.sh
+++ misc/beluga_load_env.sh
@@ -3,11 +3,13 @@
 # case 2: to submit a beluga batch job don't run this scrip directly, instead modify and run beluga_gpu.sh or beluga_gpu.sh in this folder
 
 # Load required modules
-module load python/3.6 cuda/10.1 cudnn/7.6.5
-module load nixpkgs/16.09  gcc/8.3.0 rdkit/2019.03.4
-source /lustre03/project/6004852/mkkr/anaconda3/bin/activate lz
+module load python/3.6 module load cuda/10.1 cudnn/7.6.5
+module load nixpkgs/16.09 load gcc/8.3.0 rdkit/2019.03.4
 
 # Setup CUDA paths
 export PATH=$CUDA_PATH/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_PATH/lib64
 export LIBRARY_PATH=$LIBRARY_PATH:$CUDA_PATH/lib64
+
+source /lustre03/project/6004852/mkkr/anaconda3/etc/profile.d/conda.sh
+conda activate lz
