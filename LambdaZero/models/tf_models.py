c07acca78ebc51b11f847040c6fc70bba97281d6
Moksh Jain <mokshjn00@gmail.com>
2020-07-15 12:06:11 -0400

    revert tf models

--- LambdaZero/models/tf_models.py
+++ LambdaZero/models/tf_models.py
@@ -3,14 +3,12 @@ from ray.rllib.utils import try_import_tf
 import numpy as np
 tf = try_import_tf()
 
-from ray.rllib.agents.dqn.distributional_q_tf_model import DistributionalQTFModel
+from ray.rllib.agents.dqn.distributional_q_model import DistributionalQModel
 from ray.rllib.models.tf.tf_modelv2 import TFModelV2
-from ray.rllib.utils.annotations import override
 from ray.rllib.models.tf.misc import normc_initializer
 
-# from LambdaZero.utils import RunningMeanStd
+class MolActorCritic_tfv1(DistributionalQModel, TFModelV2):
 
-class MolActorCritic_tfv1(DistributionalQTFModel, TFModelV2):
     def __init__(self,obs_space, action_space, num_outputs, model_config, name, **kw):
         super(MolActorCritic_tfv1, self).__init__(obs_space, action_space, num_outputs, model_config, name, **kw)
 
@@ -59,37 +57,6 @@ class MolActorCritic_tfv1(DistributionalQTFModel, TFModelV2):
         self.actor_critic = tf.keras.Model([mol_embed, jbond_embed, stem_embed], [action_logits, value_logit])
         self.register_variables(self.actor_critic.variables)
 
-        # RND
-        if 'rnd_weight' in model_config['custom_options'].keys():
-            self.rnd_weight = model_config['custom_options']['rnd_weight']
-        else:
-            self.rnd_weight = 0
-        # import pdb;pdb.set_trace();
-        if self.rnd_weight > 0:
-            # Target
-            rnd_target_inp = tf.keras.layers.Input(shape=(7619,))
-            rnd_target_1 = tf.keras.layers.Dense(256,
-                                           activation=tf.nn.relu,
-                                           kernel_initializer=normc_initializer(0.01))(rnd_target_inp)
-            rnd_target_out = tf.keras.layers.Dense(256,
-                                           activation=None,
-                                           kernel_initializer=normc_initializer(0.01))(rnd_target_1)
-            rnd_target_1.trainable=False
-            rnd_target_out.trainable=False
-            self.rnd_target = tf.keras.Model([rnd_target_inp], [rnd_target_out])
-            # Predictor
-            rnd_predictor_inp = tf.keras.layers.Input(shape=(7619,))
-            rnd_predictor_1 = tf.keras.layers.Dense(256,
-                                           activation=tf.nn.relu,
-                                           kernel_initializer=normc_initializer(0.01))(rnd_predictor_inp)
-            rnd_predictor_out = tf.keras.layers.Dense(256,
-                                           activation=None,
-                                           kernel_initializer=normc_initializer(0.01))(rnd_predictor_1)
-            self.rnd_predictor = tf.keras.Model([rnd_predictor_inp], [rnd_predictor_out])
-            self.flatten_obs = tf.keras.layers.Flatten()
-            # register only predictor weights to be updated by optim.
-            self.register_variables(self.rnd_predictor.variables)
-            self.register_variables(self.rnd_target.variables)
     def forward(self, input_dict, state, seq_lens):
 
         # Extract the available actions tensor from the observation.
@@ -105,28 +72,5 @@ class MolActorCritic_tfv1(DistributionalQTFModel, TFModelV2):
         inf_mask = tf.maximum(tf.log(action_mask), tf.float32.min)
         return action_logits + inf_mask, state
 
-    
-    @override(TFModelV2)
-    def custom_loss(self, policy_loss, loss_inputs):
-        if self.rnd_weight > 0:
-            obs = self.flatten_obs(loss_inputs['obs'])
-            # obs = tf.clip_by_value((obs_r - self.rnd_obs_stats.mean) / (self.rnd_obs_stats.std), -5, 5)
-            target_reward = self.rnd_target(obs)
-            predictor_reward = self.rnd_predictor(obs)
-            self.rnd_loss = tf.keras.losses.mean_squared_error(target_reward, predictor_reward)
-            # self.rnd_loss = (rnd_loss - self.rnd_rew_stats.mean) / (self.rnd_rew_stats.std)
-            self.policy_loss = policy_loss
-            # self.rnd_obs_stats.update(obs_r)
-            # self.rnd_rew_stats.update(self.rnd_loss)
-            return policy_loss + self.rnd_weight * self.rnd_loss
-        else:
-            return policy_loss
-
     def value_function(self):
         return self._value
-
-    def custom_stats(self):
-        return {
-            "rnd_loss": self.rnd_loss,
-            "policy_loss": self.policy_loss
-        }
