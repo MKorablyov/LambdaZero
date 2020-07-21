6e5c1e0fcd913f7a36aff083b213b2e1963fcac8
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-07-21 15:28:15 -0400

    Function to extract lowest energy index, and test.

--- LambdaZero/examples/env3d/rdkit_utilities.py
+++ LambdaZero/examples/env3d/rdkit_utilities.py
@@ -1,8 +1,11 @@
+from typing import List, Tuple
+
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.Chem.rdchem import Mol
-import numpy as np
 from rdkit.Geometry.rdGeometry import Point3D
+
+import numpy as np
 
 
 def get_atomic_masses(mol: Mol):
@@ -40,3 +43,28 @@ def get_mmff_energy(mol_with_hydrogens: Mol, conf_id: int = 0):
     properties = AllChem.MMFFGetMoleculeProperties(mol_with_hydrogens, mmffVariant='MMFF94')
     energy = AllChem.MMFFGetMoleculeForceField(mol_with_hydrogens, properties, confId=conf_id).CalcEnergy()
     return energy
+
+
+def find_index_of_lowest_converged_energy(list_tuples_energy_converged: List[Tuple]) -> int:
+    """
+    Finds the index of the lowest converged energy
+
+    Args:
+        list_tuples_energy_converged (list(tuple)): the output of  MMFFOptimizeMoleculeConfs
+
+    Returns:
+        lowest_converged_energy_index (int): index of lowest converged energy
+
+    """
+    mask = np.array([t[0] != 0 for t in list_tuples_energy_converged])  # a flag value of 0 means "converged"
+
+    if np.all(mask):  # nothing is converged
+        return np.NaN
+
+    energies = np.array([t[1] for t in list_tuples_energy_converged])
+
+    masked_energies = np.ma.masked_array(energies, mask=mask)
+    lowest_energy_index = np.argmin(masked_energies)
+
+    return lowest_energy_index
+
--- LambdaZero/examples/env3d/tests/test_rdkit_utilities.py
+++ LambdaZero/examples/env3d/tests/test_rdkit_utilities.py
@@ -6,33 +6,19 @@ from LambdaZero.examples.env3d.rdkit_utilities import (
 )
 
 
-@pytest.fixture
-def energy_converged_tuples():
+list_clean_tuples = [(1., 1.), (0., 3.), (1, 3.), (0., 2.), (1., 1.)]
+clean_index = 3
 
-    list_energies = [1., 3., 3., 2., 1.]
-    list_converged = [1, 0, 1, 0, 1]
-
-    list_tuples = [(c, e) for c, e in zip(list_converged, list_energies)]
-
-    return list_tuples
+list_bad_tuples = [(1., 1.), (1., 2.)]
+bad_index = np.NaN
 
 
-@pytest.fixture
-def expected_lowest_energy_index(energy_converged_tuples):
-    converged_index = None
-    lowest_energy = np.inf
-    for index, (converged, energy) in enumerate(energy_converged_tuples):
-        if converged == 0 and energy < lowest_energy:
-            converged_index = index
-            lowest_energy = energy
-
-    return converged_index
-
-
+@pytest.mark.parametrize("energy_converged_tuples, expected_lowest_energy_index",
+                         [(list_clean_tuples, clean_index), (list_bad_tuples, bad_index)])
 def test_find_index_of_lowest_converged_energy(
     energy_converged_tuples, expected_lowest_energy_index
 ):
     computed_lowest_energy_index = find_index_of_lowest_converged_energy(
         energy_converged_tuples
     )
-    assert computed_lowest_energy_index == expected_lowest_energy_index
+    np.testing.assert_equal(computed_lowest_energy_index, expected_lowest_energy_index)
