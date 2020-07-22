a984f09d25d256ad47768bcbad1b1f6956751f8d
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-07-22 15:48:57 -0400

    black.

--- LambdaZero/examples/env3d/tests/test_processing.py
+++ LambdaZero/examples/env3d/tests/test_processing.py
@@ -2,11 +2,10 @@ import logging
 import tempfile
 from copy import copy
 from pathlib import Path
-import torch
 
-import pytest
 import numpy as np
 import pandas as pd
+import pytest
 import ray
 from rdkit.Chem.rdmolfiles import MolFromSmiles
 
@@ -16,19 +15,21 @@ from LambdaZero.inputs import BrutalDock
 
 @pytest.fixture
 def smiles():
-    list_smiles = ['CC(C)=CC(C)(C)O',
-                   'CC(C)=CC(=O)NC(C#N)P(=O)(O)O',
-                   'O=[SH](=O)S(=O)(=O)O',
-                   'CC(C)=CN1CCN(P(=O)(O)O)CC1',
-                   'CC(C)(O)C(F)(F)F',
-                   'c1ccc2cc(N3CCOCC3)ccc2c1',
-                   'CC(C)(O)Br',
-                   'CC(=O)N[SH](=O)=O',
-                   'CC(C)=CC1CC(C(NC(=O)C(C)O)NC(=O)S(=O)(=O)O)N(c2ccc3ccccc3c2)C1C(C)C',
-                   'C1=C(c2ccc[nH]2)CCCC1',
-                   'O=C(NCF)C1=CCCCC1',
-                   'CC(=Cc1cc[nH]c1)CCCl',
-                   'CC(=O)NC(=O)NC1=CN(I)C=CC1']
+    list_smiles = [
+        "CC(C)=CC(C)(C)O",
+        "CC(C)=CC(=O)NC(C#N)P(=O)(O)O",
+        "O=[SH](=O)S(=O)(=O)O",
+        "CC(C)=CN1CCN(P(=O)(O)O)CC1",
+        "CC(C)(O)C(F)(F)F",
+        "c1ccc2cc(N3CCOCC3)ccc2c1",
+        "CC(C)(O)Br",
+        "CC(=O)N[SH](=O)=O",
+        "CC(C)=CC1CC(C(NC(=O)C(C)O)NC(=O)S(=O)(=O)O)N(c2ccc3ccccc3c2)C1C(C)C",
+        "C1=C(c2ccc[nH]2)CCCC1",
+        "O=C(NCF)C1=CCCCC1",
+        "CC(=Cc1cc[nH]c1)CCCl",
+        "CC(=O)NC(=O)NC1=CN(I)C=CC1",
+    ]
     return list_smiles
 
 
@@ -47,16 +48,18 @@ def data_df(smiles):
         n_axis = np.random.rand(3)
         n_axis /= np.linalg.norm(n_axis)
 
-        theta = 2*np.pi*np.random.rand()
+        theta = 2 * np.pi * np.random.rand()
         block_index = np.random.randint(0, 105)
         attachment_node_index = np.random.randint(0, number_of_atoms)
 
-        row = {'smi': s,
-               'coord': coord,
-               'n_axis': n_axis,
-               'attachment_node_index': attachment_node_index,
-               'attachment_angle': theta,
-               "attachment_block_index": block_index}
+        row = {
+            "smi": s,
+            "coord": coord,
+            "n_axis": n_axis,
+            "attachment_node_index": attachment_node_index,
+            "attachment_angle": theta,
+            "attachment_block_index": block_index,
+        }
 
         list_rows.append(row)
 
@@ -67,18 +70,18 @@ def data_df(smiles):
 
 @pytest.fixture
 def dataset_root_and_filename(data_df):
-    filename = 'test_data'
+    filename = "test_data"
 
     df_for_writing = copy(data_df)
-    df_for_writing['coord'] = df_for_writing['coord'].apply(np.ndarray.tobytes)
-    df_for_writing['n_axis'] = df_for_writing['n_axis'].apply(np.ndarray.tobytes)
+    df_for_writing["coord"] = df_for_writing["coord"].apply(np.ndarray.tobytes)
+    df_for_writing["n_axis"] = df_for_writing["n_axis"].apply(np.ndarray.tobytes)
 
     with tempfile.TemporaryDirectory() as root_dir:
         logging.info("creating a fake directory")
-        raw_dir = Path(root_dir).joinpath('raw')
+        raw_dir = Path(root_dir).joinpath("raw")
         raw_dir.mkdir()
 
-        raw_data_path = raw_dir.joinpath(filename+'.feather')
+        raw_data_path = raw_dir.joinpath(filename + ".feather")
 
         df_for_writing.to_feather(raw_data_path)
 
@@ -90,16 +93,25 @@ def test_env3d_proc(dataset_root_and_filename, data_df):
 
     root_directory, data_filename = dataset_root_and_filename
 
-    props = ['coord', 'n_axis', 'attachment_node_index', 'attachment_angle', "attachment_block_index"]
+    props = [
+        "coord",
+        "n_axis",
+        "attachment_node_index",
+        "attachment_angle",
+        "attachment_block_index",
+    ]
 
     ray.init(local_mode=True)
 
-    dataset = BrutalDock(root_directory,
-                         props=props,
-                         file_names=[data_filename],
-                         proc_func=env3d_proc)
+    dataset = BrutalDock(
+        root_directory, props=props, file_names=[data_filename], proc_func=env3d_proc
+    )
 
-    for property in ['attachment_node_index', 'attachment_angle', "attachment_block_index"]:
+    for property in [
+        "attachment_node_index",
+        "attachment_angle",
+        "attachment_block_index",
+    ]:
         expected_values = data_df[property].values
         computed_values = dataset.data[property].numpy()
         np.testing.assert_allclose(expected_values, computed_values)
@@ -107,10 +119,10 @@ def test_env3d_proc(dataset_root_and_filename, data_df):
     for row_index, graph in enumerate(dataset):
         row = data_df.iloc[row_index]
 
-        expected_coords = row['coord']
+        expected_coords = row["coord"]
         computed_coords = graph.pos.numpy()
         np.testing.assert_allclose(expected_coords, computed_coords)
 
-        expected_n_axis = row['n_axis']
-        computed_n_axis = graph['n_axis'].numpy()[0]
+        expected_n_axis = row["n_axis"]
+        computed_n_axis = graph["n_axis"].numpy()[0]
         np.testing.assert_allclose(expected_n_axis, computed_n_axis)
