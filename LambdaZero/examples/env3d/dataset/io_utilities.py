87362bbe7f07725d96aadb7b0bd09c94978f1149
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-07-22 16:49:41 -0400

    Code to append rows to feather file.

--- LambdaZero/examples/env3d/dataset/io_utilities.py
+++ LambdaZero/examples/env3d/dataset/io_utilities.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import Dict
 import pandas as pd
 
@@ -22,3 +23,14 @@ def process_row_for_writing_to_feather(row: Dict) -> Dict:
 
 def append_row_to_dataframe(df: pd.DataFrame, row: Dict) -> pd.DataFrame:
     return df.append(row, ignore_index=True)
+
+
+def create_or_append_feather_file(feather_file_path: Path, row):
+
+    if feather_file_path.exists():
+        df = pd.read_feather(feather_file_path)
+        new_df = append_row_to_dataframe(df, row)
+    else:
+        new_df = pd.DataFrame(data=row, index=[0])
+
+    new_df.to_feather(str(feather_file_path))
--- LambdaZero/examples/env3d/tests/test_io_utilities.py
+++ LambdaZero/examples/env3d/tests/test_io_utilities.py
@@ -1,8 +1,14 @@
+import tempfile
+from pathlib import Path
+
 import pytest
 import numpy as np
 import pandas as pd
 
-from LambdaZero.examples.env3d.dataset.io_utilities import append_row_to_dataframe
+from LambdaZero.examples.env3d.dataset.io_utilities import (
+    append_row_to_dataframe,
+    create_or_append_feather_file,
+)
 
 
 @pytest.fixture
@@ -13,10 +19,28 @@ def dataframe():
     return df
 
 
+@pytest.fixture
+def feather_path():
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        feather_file_path = Path(tmp_dir).joinpath("test.feather")
+        yield feather_file_path
+
+
 def test_append_row_to_dataframe(dataframe):
 
     df = dataframe.iloc[:-1]
     last_row = dataframe.iloc[-1].to_dict()
     computed_df = append_row_to_dataframe(df, last_row)
+
+    pd.testing.assert_frame_equal(computed_df, dataframe)
+
+
+def test_create_or_append_feather_file(feather_path, dataframe):
+
+    for index, row_series in dataframe.iterrows():
+        row = row_series.to_dict()
+        create_or_append_feather_file(feather_path, row)
+
+    computed_df = pd.read_feather(feather_path)
 
     pd.testing.assert_frame_equal(computed_df, dataframe)
