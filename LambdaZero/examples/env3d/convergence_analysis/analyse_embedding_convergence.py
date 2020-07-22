5e0809e74fe71cc1450051a2a7fcd242b7eaba50
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-07-22 10:19:50 -0400

    Capture exception so code doesn't crash.

--- LambdaZero/examples/env3d/convergence_analysis/analyse_embedding_convergence.py
+++ LambdaZero/examples/env3d/convergence_analysis/analyse_embedding_convergence.py
@@ -14,7 +14,6 @@ from rdkit.Chem.rdchem import Mol
 from tqdm import tqdm
 
 import LambdaZero.utils
-from LambdaZero.chem import mol_from_frag
 from LambdaZero.environments.molMDP import MolMDP
 from LambdaZero.examples.env3d.rdkit_utilities import (
     get_lowest_energy_and_mol_with_hydrogen,
@@ -41,9 +40,14 @@ def generate_minimum_energy_vs_number_of_configuration_profile(
 
     list_rows = []
     for num_conf in tqdm(list_num_conf, desc="EMBED_CONFIG", file=sys.stdout):
-        min_energy, mol_with_hydrogen, number_of_successes = get_lowest_energy_and_mol_with_hydrogen(
-            mol, num_conf, max_iters, random_seed
-        )
+        try:
+            min_energy, mol_with_hydrogen, number_of_successes = get_lowest_energy_and_mol_with_hydrogen(
+                mol, num_conf, max_iters, random_seed
+            )
+        except ValueError as e:
+            min_energy = np.NaN
+            number_of_successes = 0
+            print(f"\n{e}\n")
 
         row = {
             "smiles": smiles,
@@ -64,8 +68,9 @@ results_dir = Path(summaries_dir).joinpath("env3d")
 # computation parameters
 number_of_blocks = 10
 random_seed = 12312
-list_num_conf = [1, 10, 25, 50, 100, 150, 200]
-number_of_molecules = 100
+#list_num_conf = [1, 10, 25, 50, 100, 150, 200]
+list_num_conf = [1, 10, 25]
+number_of_molecules = 10
 
 results_path = Path(results_dir).joinpath(
     f"num_conf_convergence/conv_{number_of_blocks}_blocks.pkl"
@@ -83,12 +88,7 @@ if __name__ == "__main__":
 
         molMDP.reset()
         molMDP.random_walk(number_of_blocks)
-
-        mol, mol_bond = mol_from_frag(
-            jun_bonds=molMDP.molecule.jbonds,
-            frags=molMDP.molecule.blocks,
-            optimize=False,
-        )
+        mol = molMDP.molecule.mol
 
         df = generate_minimum_energy_vs_number_of_configuration_profile(
             mol, list_num_conf, random_seed=random_seed
