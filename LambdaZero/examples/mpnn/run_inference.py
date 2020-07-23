c52ce97093c92b31c1988e1ad93d509dcf929150
Victor Butoi <victor.butoi@gmail.com>
2020-07-23 04:32:53 -0400

    inference fixed

--- LambdaZero/examples/mpnn/run_inference.py
+++ LambdaZero/examples/mpnn/run_inference.py
@@ -20,14 +20,16 @@ datasets_dir, programs_dir, summaries_dir = get_external_dirs()
 
 config = {
     "trainer_config": {
-        "dataset_root": "/home/vbutoi/scratch/zinc_data",
-        "targets": ["gridscore"],
-        "target_norms": [[-26.3, 12.3]],
+        "dataset_root": "/home/nova/zinc_data",
+        "targets": "gridscore",
+        "target_norm": [-43.042, 7.057],
         "file_names": ["zinc15_full_2", "zinc15_full_46", "zinc15_full_13",
-                       "zinc15_full_31", "zinc15_full_47", "zinc15_full_16",
+                       "zinc15_full_31", "zinc15_full_47", "zinc15_full_16"],
+                       """
                        "zinc15_full_33", "zinc15_full_48", "zinc15_full_26",
                        "zinc15_full_38", "zinc15_full_54", "zinc15_full_28",
                        "zinc15_full_40"],
+                       """
         "transform": transform,
         "lr": 0.001,
         "b_size": 64,
@@ -44,18 +46,18 @@ ray.init()
 device = th.device('cuda' if th.cuda.is_available() else 'cpu')
 
 model = LambdaZero.models.MPNNet()
-model.load_state_dict(th.load('/home/vbutoi/LambdaZero/summaries/knn_split/mpnn000_L2/BasicRegressor_26b1482e_0_2020-07-21_02-05-17y98a028f/checkpoint_100/model.pth'))
+model.load_state_dict(th.load('/home/nova/KNN/mpnn000_L2/BasicRegressor_26b1482e_0_2020-07-21_02-05-17y98a028f/checkpoint_100/model.pth'))
 model.to(device)
 model.eval()
 
 config = config['trainer_config']
 
 target = config["targets"][0]
-target_norm = config["target_norms"][0]
+target_norm = config["target_norm"]
 
 dataset = LambdaZero.inputs.BrutalDock(config["dataset_root"],
                                         props=config["molprops"],
-                                        transform=config["transform"],
+                                        transform=LambdaZero.utils.Complete(),
                                         file_names=config["file_names"])
 # split dataset
 bsize = 128
@@ -63,18 +65,15 @@ bsize = 128
 val_subset = dataset
 loader = DL(val_subset, batch_size=bsize)
 
-mol_checkpoints = []
-regrets = []
-med_regrets = []
-
-running_preds_true_energy = th.tensor([],requires_grad=False)
-running_preds = th.tensor([],requires_grad=False)
-running_gc = th.tensor([],requires_grad=False)
-
+normalizer = LambdaZero.utils.MeanVarianceNormalizer(config["target_norm"])
 num_mol_seen = 0
 
-real_energies = []
-predictions = []
+epoch_targets = []
+epoch_preds = []
+
+top_fifteen = []
+top_fifty = []
+mol_seen = []
 
 print("About to start")
 i = 0
@@ -84,83 +83,33 @@ for bidx, data in enumerate(loader):
     print("Current iter: " + str(i))
     i += 1
     data = data.to(device)
+    targets = getattr(data, config["targets"])
 
     with th.no_grad():
-        logit = model(data)
-
-    pred = (logit * target_norm[1]) + target_norm[0]
-
-    real_energies.append(data.gridscore.item())
-    predictions.append(pred.item())
+        logits = model(data)
 
     num_mol_seen += bsize
+    epoch_targets.append(targets.detach().cpu().numpy())
+    epoch_preds.append(normalizer.unnormalize(logits).detach().cpu().numpy())
 
-    y = getattr(data, target)
+    e_targ = np.concatenate(epoch_targets, 0)
+    e_pred = np.concatenate(epoch_preds, 0)
 
-    cpu_y = y.cpu()
-    cpu_pred = pred.cpu()
+    ranked_targets = e_targ[np.argsort(e_targ)]
+    predsranked_targets = e_pred[np.argsort(epoch_preds)]
 
-    highest_ys = th.topk(y.cpu(), config['num_rank'])
-    running_gc = th.cat((running_gc, highest_ys[0]), 0)
-    running_gc = th.topk(running_gc, config['num_rank'])[0]
+    top_fifteen.append(np.median(predsranked_targets[:15]) - np.median(ranked_targets[:15]))
+    top_fifty.append(np.median(predsranked_targets[:50]) - np.median(ranked_targets[:50]))
+    mol_seen.append(num_mol_seen)
 
-    highest_preds = th.topk(cpu_pred, config['num_rank'])
-
-    running_preds = th.cat((running_preds, highest_preds[0]), 0)
-    running_preds_true_energy = th.cat((running_preds_true_energy, cpu_y[highest_preds[1]]), 0)
-
-    running_preds = th.topk(running_preds, config['num_rank'])
-
-    running_preds_true_energy = running_preds_true_energy[running_preds[1]].detach()
-    running_preds = running_preds[0].detach()
-
-    median_regret = abs(np.median(running_preds_true_energy) - np.median(running_gc)).item()
-    regret = F.l1_loss(running_preds_true_energy, running_gc).item()
-
-    regrets.append(regret)
-    mol_checkpoints.append(num_mol_seen)
-    med_regrets.append(median_regret)
-
-plt.title("Predictions vs Real")
-plt.xlabel("Real Energies")
-plt.ylabel("Predictions")
-plt.scatter(real_energies, predictions)
-plt.savefig("/home/vbutoi/scratch/charts/pred_vs_real.png")
-
-plt.title("Regret Over Time")
+plt.title("Num Mols Seen vs Top 15 Med Regret")
 plt.xlabel("Num Mols Seen")
-plt.ylabel("MSE Regret")
-plt.plot(num_mol_seen, regrets, label="mae regret")
-plt.plot(num_mol_seen, med_regrets, label="median regret")
-plt.savefig("/home/vbutoi/scratch/charts/regret_over_time.png")
+plt.ylabel("Regret")
+plt.scatter(mol_seen, top_fifteen)
+plt.savefig("/home/nova/charts/regret_over_time15.png")
 
-"""
-    num_mol_seen += bsize
-
-    pred = (logit * target_norm[1]) + target_norm[0]
-    y = getattr(data, target)
-
-    cpu_y = y.cpu()
-    cpu_pred = pred.cpu()
-
-    highest_ys = th.topk(y.cpu(), config['num_rank'])i
-    running_gc = th.cat((running_gc, highest_ys[0]), 0)
-    running_gc = th.topk(running_gc, config['num_rank'])[0]
-
-    highest_preds = th.topk(cpu_pred, config['num_rank'])
-
-    running_preds = th.cat((running_preds, highest_preds[0]), 0)
-    running_preds_true_energy = th.cat((running_preds_true_energy, cpu_y[highest_preds[1]]), 0)
-
-    running_preds = th.topk(running_preds, config['num_rank'])
-
-    running_preds_true_energy = running_preds_true_energy[running_preds[1]].detach()
-    running_preds = running_preds[0].detach()
-
-    median_regret = abs(np.median(running_preds_true_energy) - np.median(running_gc)).item()
-    regret = F.l1_loss(running_preds_true_energy, running_gc).item()
-    mse_regret = F.mse_loss(running_preds_true_energy, running_gc).item()
-
-    regrets.append(regret)
-    mol_checkpoints.append(num_mol_seen)
-"""
+plt.title("Num Mols Seen vs Top 50 Med Regret")
+plt.xlabel("Num Mols Seen")
+plt.ylabel("Regret")
+plt.plot(mol_seen, top_fifty)
+plt.savefig("/home/nova/charts/regret_over_time50.png")
