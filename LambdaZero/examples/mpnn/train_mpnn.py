cccefd0eb38c41b1f9c20de51db089a809b3e333
Victor Butoi <vbutoi@beluga4.int.ets1.calculquebec.ca>
2020-07-23 19:12:22 -0400

    fixed file path

--- LambdaZero/examples/mpnn/train_mpnn.py
+++ LambdaZero/examples/mpnn/train_mpnn.py
@@ -11,7 +11,7 @@ import ray
 from ray import tune
 from ray.rllib.utils import merge_dicts
 
-from .dimenet import DimeNet
+from dimenet import DimeNet
 
 from LambdaZero.utils import get_external_dirs, BasicRegressor
 import LambdaZero.inputs
