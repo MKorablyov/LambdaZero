85150a572665ec18fbddb8c30b0f13d343bababd
Victor Butoi <victor.butoi@gmail.com>
2020-07-23 17:17:50 -0400

    dimenet additions

--- LambdaZero/examples/mpnn/dimenet.py
+++ LambdaZero/examples/mpnn/dimenet.py
@@ -1,6 +1,5 @@
 import os
 import os.path as osp
-from math import sqrt, pi as PI
 
 import numpy as np
 import torch
@@ -11,9 +10,63 @@ from torch_geometric.nn import radius_graph
 from torch_geometric.data import download_url
 from torch_geometric.data.makedirs import makedirs
 
-from ..acts import swish
-from ..inits import glorot_orthogonal
-from LambdaZero.examples.mpnn.dime_net.dimenet_utils import bessel_basis, real_sph_harm
+from dimenet_utils import bessel_basis, real_sph_harm
+import math
+
+def uniform(size, tensor):
+    bound = 1.0 / math.sqrt(size)
+    if tensor is not None:
+        tensor.data.uniform_(-bound, bound)
+
+
+def kaiming_uniform(tensor, fan, a):
+    if tensor is not None:
+        bound = math.sqrt(6 / ((1 + a**2) * fan))
+        tensor.data.uniform_(-bound, bound)
+
+
+def glorot(tensor):
+    if tensor is not None:
+        stdv = math.sqrt(6.0 / (tensor.size(-2) + tensor.size(-1)))
+        tensor.data.uniform_(-stdv, stdv)
+
+
+def glorot_orthogonal(tensor, scale):
+    if tensor is not None:
+        torch.nn.init.orthogonal_(tensor.data)
+        scale /= ((tensor.size(-2) + tensor.size(-1)) * tensor.var())
+        tensor.data *= scale.sqrt()
+
+
+def zeros(tensor):
+    if tensor is not None:
+        tensor.data.fill_(0)
+
+
+def ones(tensor):
+    if tensor is not None:
+        tensor.data.fill_(1)
+
+
+def normal(tensor, mean, std):
+    if tensor is not None:
+        tensor.data.normal_(mean, std)
+
+
+def reset(nn):
+    def _reset(item):
+        if hasattr(item, 'reset_parameters'):
+            item.reset_parameters()
+
+    if nn is not None:
+        if hasattr(nn, 'children') and len(list(nn.children())) > 0:
+            for item in nn.children():
+                _reset(item)
+        else:
+            _reset(nn)
+
+def swish(x):
+    return x * x.sigmoid()
 
 try:
     import sympy as sym
