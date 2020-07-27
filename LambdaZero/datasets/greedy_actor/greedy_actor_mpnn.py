a4f55524f291ee32b29e26c02d556ca4c4fdfb34
sumanabasu <sumana.basu21@gmail.com>
2020-07-27 03:42:31 -0400

    reproduce pyarrow error

--- LambdaZero/datasets/greedy_actor/greedy_actor_mpnn.py
+++ LambdaZero/datasets/greedy_actor/greedy_actor_mpnn.py
@@ -13,6 +13,7 @@ import numpy as np
 import os
 import os.path as osp
 import pandas as pd
+import pyarrow.feather as feather
 import ray
 import time
 import torch
@@ -26,10 +27,10 @@ class cfg:
 	datasets_dir, programs_dir, summaries_dir = LambdaZero.utils.get_external_dirs()
 	
 	num_cpus = 4
-	num_gpus = 1
-	device = "cuda"
+	num_gpus = 0
+	device = "cpu"
 	
-	db_name = "actor_dock_100k"
+	db_name = "actor_dock"
 	db_path = osp.join(datasets_dir, db_name)
 	if not osp.exists(db_path):
 		os.mkdir(db_path)
@@ -44,7 +45,7 @@ class cfg:
 	dockscore_model = osp.join(datasets_dir, "brutal_dock/d4/dock_blocks105_walk40_12_clust_model002")
 	
 	# dataset size
-	dataset_len = 100000
+	dataset_len = 3
 	
 	# data parallel
 	num_workers = 2
@@ -97,12 +98,12 @@ class Worker:
 				reward = self.comp_reward(self.molMDP.molecule, env_stop=False, simulate=True, num_steps=1)[0]
 				addblock_values.append(reward)
 				self.molMDP.molecule = deepcopy(init_molecule)
-			action_values.append(addblock_values)
+			action_values.append(np.array(addblock_values)) #TODO: try flatten
 			
 		return(Chem.MolToSmiles(self.molMDP.molecule.mol),
 			   self.comp_reward(self.molMDP.molecule, env_stop=False, simulate=True, num_steps=1)[0],
-			  self.molMDP.molecule.stem_atmidxs,
-			   action_values)
+			  np.asarray(self.molMDP.molecule.stem_atmidxs).astype('int8'),
+			   np.array(action_values).flatten()) #TODO: remove astype
 	
 	#TODO request that molecule object should provide torch graph and smiles
 
@@ -131,12 +132,14 @@ def generate_dataset():
 		task2worker[new_task] = done_worker
 		tasks.append(new_task)
 		
-		if (i % 10) == 0:
-			df = pd.DataFrame(samples, columns=('smile', 'initial_energry', 'stem_idxs', 'docking_energies'))
-			df.to_parquet(osp.join(cfg.results_dir, cfg.db_name) + ".feather")
+		if (i % 500) == 0:
+			df = pd.DataFrame(samples, columns=('smile', 'initial_energy', 'stem_idxs', 'docking_energies'))
+			# feather.write_feather(df, (osp.join(cfg.results_dir, cfg.db_name) + ".feather"))
+			df.to_feather(osp.join(cfg.results_dir, cfg.db_name) + ".feather")
 	
-	df = pd.DataFrame(samples, columns=('smile', 'initial_energry', 'stem_idxs', 'docking_energies'))
-	df.to_parquet(osp.join(cfg.results_dir, cfg.db_name) + ".feather")
+	df = pd.DataFrame(samples, columns=('smile', 'initial_energy', 'stem_idxs', 'docking_energies'))
+	# feather.write_feather(df, (osp.join(cfg.results_dir, cfg.db_name) + ".feather"))
+	df.to_feather(osp.join(cfg.results_dir, cfg.db_name) + ".feather")
 	
 
 def split_dataset():
@@ -145,7 +148,7 @@ def split_dataset():
 	:return: stores the indices of each set as a list of [train_idx, valid_idx, test_idx]
 	'''
 	print('Splitting :', osp.join(cfg.results_dir, cfg.db_name) + ".feather")
-	data = pd.read_parquet(osp.join(cfg.results_dir, cfg.db_name) + ".feather")
+	data = pd.read_feather(osp.join(cfg.results_dir, cfg.db_name) + ".feather")
 	splits = LambdaZero.inputs.random_split(len(data), cfg.split_probs)
 
 	split_path = osp.join(cfg.results_dir, cfg.split_name + ".npy")
@@ -155,6 +158,6 @@ def split_dataset():
 if __name__ == "__main__":
 	generate_dataset()
 	# time.sleep(120)
-	split_dataset()
+	# split_dataset()
 
 
