46b23aaa725a329c834ecc18870b2d0a817f2031
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-07-28 21:51:07 -0400

    Generate data using ray.

--- LambdaZero/examples/env3d/dataset/main.py
+++ LambdaZero/examples/env3d/dataset/main.py
@@ -1,19 +1,16 @@
 """
 The goal of this script is to generate the dataset of molecules embedded in 3D space.
-
-# TODO: we should use ray to parallelize effectively here. It will help if we want to scale to a very large dataset.
 """
 import json
 import os
 from pathlib import Path
 
 import numpy as np
+import ray
 
 import LambdaZero.utils
-from LambdaZero.environments.molMDP import MolMDP
-from LambdaZero.examples.env3d.dataset.data_generation import get_data_row
+from LambdaZero.examples.env3d.dataset.data_row_generator import DataRowGenerator
 from LambdaZero.examples.env3d.dataset.io_utilities import (
-    process_row_for_writing_to_feather,
     create_or_append_feather_file, get_debug_blocks,
 )
 
@@ -22,57 +19,57 @@ blocks_file = os.path.join(datasets_dir, "fragdb/blocks_PDB_105.json")
 results_dir = Path(summaries_dir).joinpath("env3d/dataset/")
 results_dir.mkdir(exist_ok=True)
 
-
 debug_flag = False
 
-random_seed = 12312
+master_random_seed = 12312
+num_cpus = 12
+
+if debug_flag:  # Small parameters for quick execution
+    number_of_parent_blocks = 3
+    number_of_blocks = 10
+    num_conf = 50
+    max_iters = 200
+    max_number_of_molecules = 24
+    output_filename = f"env3d_dataset_{number_of_parent_blocks}_parent_blocks_debug.feather"
+
+    debug_blocks = get_debug_blocks(blocks_file, number_of_blocks)
+    blocks_file = results_dir.joinpath("blocks_debug.json")
+    with open(blocks_file, 'w') as f:
+        json.dump(debug_blocks, f)
+
+else:
+    number_of_parent_blocks = 5
+    num_conf = 25
+    max_iters = 200
+    max_number_of_molecules = 1000
+    output_filename = f"env3d_dataset_{number_of_parent_blocks}_parent_blocks.feather"
+
+output_path = results_dir.joinpath(output_filename)
 
 if __name__ == "__main__":
-    np.random.seed(random_seed)
 
-    if debug_flag:  #  Small parameters for quick execution
-        number_of_parent_blocks = 3
-        number_of_blocks = 10
-        num_conf = 50
-        max_iters = 200
-        max_number_of_molecules = 10
-        output_filename = f"env3d_dataset_{number_of_parent_blocks}_parent_blocks_debug.feather"
+    ray.init(num_cpus=num_cpus)
 
-        debug_blocks = get_debug_blocks(blocks_file, number_of_blocks)
-        blocks_file = results_dir.joinpath("blocks_debug.json")
-        with open(blocks_file, 'w') as f:
-            json.dump(debug_blocks, f)
+    np.random.seed(master_random_seed)
 
-    else:
-        number_of_parent_blocks = 5
-        num_conf = 25
-        max_iters = 200
-        max_number_of_molecules = 1000
-        output_filename = f"env3d_dataset_{number_of_parent_blocks}_parent_blocks.feather"
+    generators = []
 
-    output_path = results_dir.joinpath(output_filename)
+    for _ in range(num_cpus):
+        random_seed = np.random.randint(1e9)
+        g = DataRowGenerator.remote(blocks_file, number_of_parent_blocks, num_conf, max_iters, random_seed)
+        generators.append(g)
 
-    reference_molMDP = MolMDP(blocks_file=blocks_file)
+    row_ids = [generators[i % num_cpus].generate_row.remote() for i in range(max_number_of_molecules)]
 
-    for index in range(max_number_of_molecules):
-        print(f" Computing molecule {index} of {max_number_of_molecules}")
-        reference_molMDP.reset()
-        reference_molMDP.random_walk(number_of_parent_blocks)
-        number_of_stems = len(reference_molMDP.molecule.stems)
+    done_count = 0
+    while row_ids:
+        done_ids, row_ids = ray.wait(row_ids)
+        done_count += len(done_ids)
+        print(f"Done {done_count} out of {max_number_of_molecules}")
 
-        if number_of_stems < 1:
-            print("no stems! Moving on to next molecule")
-            continue
-
-        attachment_stem_idx = np.random.choice(number_of_stems)
-
-        try:
-            row = get_data_row(
-                reference_molMDP, attachment_stem_idx, num_conf, max_iters, random_seed
-            )
-        except:
-            print("Something went wrong while relaxing molecule. Moving on to next molecule")
-            continue
-
-        byte_row = process_row_for_writing_to_feather(row)
-        create_or_append_feather_file(output_path, byte_row)
+        for done_id in done_ids:
+            try:
+                byte_row = ray.get(done_id)
+                create_or_append_feather_file(output_path, byte_row)
+            except:
+                print('Something went wrong with molecule generation. Moving on.')
