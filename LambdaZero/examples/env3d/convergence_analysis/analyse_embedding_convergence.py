63ed936d2d6cc1be8793adfe4721ab359ea17fa9
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-07-30 17:10:54 -0400

    Generate convergence data.

--- LambdaZero/examples/env3d/convergence_analysis/analyse_embedding_convergence.py
+++ LambdaZero/examples/env3d/convergence_analysis/analyse_embedding_convergence.py
@@ -10,8 +10,13 @@ import pandas as pd
 
 import LambdaZero.utils
 from LambdaZero.environments.molMDP import MolMDP
-from LambdaZero.examples.env3d.convergence_analysis.utils import \
-    generate_minimum_energy_vs_number_of_configuration_profile
+from LambdaZero.examples.env3d.dataset.data_generation import get_blocks_embedding_energies, \
+    compute_parent_and_all_children_energies
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
 
 datasets_dir, _, summaries_dir = LambdaZero.utils.get_external_dirs()
 blocks_file = os.path.join(datasets_dir, "fragdb/blocks_PDB_105.json")
@@ -20,32 +25,50 @@ results_dir = Path(summaries_dir).joinpath("env3d")
 
 # computation parameters
 number_of_blocks = 5
+
 random_seed = 12312
-list_num_conf = [1, 10, 25, 50, 100, 150, 200]
-number_of_molecules = 100
+list_num_conf = [10, 20, 40]
+number_of_molecules = 10
 
 results_path = Path(results_dir).joinpath(
-    f"num_conf_convergence/conv_{number_of_blocks}_blocks.pkl"
+    f"num_conf_convergence/convergence_{number_of_blocks}_blocks.pkl"
 )
 results_path.parent.mkdir(exist_ok=True)
+
+
+max_iters = 200
 
 if __name__ == "__main__":
 
     np.random.seed(random_seed)
-    molMDP = MolMDP(blocks_file=blocks_file)
+    child_block_energies_dict = get_blocks_embedding_energies(blocks_file)
+
+    reference_molMDP = MolMDP(blocks_file=blocks_file)
 
     list_df = []
-    for i in range(1, number_of_molecules + 1):
-        print(f"Molecule {i} of {number_of_molecules}")
+    for _ in range(number_of_molecules):
 
-        molMDP.reset()
-        molMDP.random_walk(number_of_blocks)
-        mol = molMDP.molecule.mol
+        reference_molMDP.reset()
+        reference_molMDP.random_walk(number_of_blocks)
 
-        df = generate_minimum_energy_vs_number_of_configuration_profile(
-            mol, list_num_conf, random_seed=random_seed
-        )
-        list_df.append(df)
+        number_of_stems = len(reference_molMDP.molecule.stems)
+        if number_of_stems < 1:
+            raise ValueError("no stems! Cannot proceed")
 
-    df = pd.concat(list_df).reset_index(drop=True)
-    df.to_pickle(results_path)
+        attachment_stem_idx = np.random.choice(number_of_stems)
+
+        for num_conf in list_num_conf:
+            df, _ = compute_parent_and_all_children_energies(
+                reference_molMDP,
+                attachment_stem_idx,
+                child_block_energies_dict,
+                num_conf,
+                max_iters,
+                random_seed)
+
+            df['num_conf'] = num_conf
+            list_df.append(df)
+
+    convergence_df = pd.concat(list_df).reset_index(drop=True)
+
+    convergence_df.to_pickle(results_path)
