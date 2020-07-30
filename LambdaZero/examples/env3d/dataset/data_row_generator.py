b8ba8d374473ae0ef8a48c14e7f2f9b4761218a1
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-07-30 15:05:27 -0400

    Generate child block energies only once per actor. Modify API consequently.

--- LambdaZero/examples/env3d/dataset/data_row_generator.py
+++ LambdaZero/examples/env3d/dataset/data_row_generator.py
@@ -5,8 +5,13 @@ from pathlib import Path
 import ray
 import numpy as np
 from LambdaZero.environments import MolMDP
-from LambdaZero.examples.env3d.dataset.data_generation import get_data_row
-from LambdaZero.examples.env3d.dataset.io_utilities import process_row_for_writing_to_feather
+from LambdaZero.examples.env3d.dataset.data_generation import (
+    get_data_row,
+    get_blocks_embedding_energies,
+)
+from LambdaZero.examples.env3d.dataset.io_utilities import (
+    process_row_for_writing_to_feather,
+)
 
 
 @ray.remote
@@ -15,8 +20,15 @@ class DataRowGenerator:
     This class drives the generation of the dataset, one row at a time, under the control of ray.
     """
 
-    def __init__(self, blocks_file: str, number_of_parent_blocks: int,
-                 num_conf: int, max_iters: int, random_seed: int, logging_directory: Path):
+    def __init__(
+        self,
+        blocks_file: str,
+        number_of_parent_blocks: int,
+        num_conf: int,
+        max_iters: int,
+        random_seed: int,
+        logging_directory: Path,
+    ):
         """
         Initialize the data generator.
 
@@ -35,10 +47,13 @@ class DataRowGenerator:
         self.max_iters = max_iters
         self.number_of_parent_blocks = number_of_parent_blocks
         self.reference_molMDP = MolMDP(blocks_file=blocks_file)
+        self.child_block_energies_dict = get_blocks_embedding_energies(blocks_file)
 
         self.logger = logging.getLogger(__name__)
-        log_file_name = str(logging_directory.joinpath(f'info_ACTOR_seed_{random_seed}.log'))
-        logging.basicConfig(filename=log_file_name, filemode='w', level=logging.INFO)
+        log_file_name = str(
+            logging_directory.joinpath(f"info_ACTOR_seed_{random_seed}.log")
+        )
+        logging.basicConfig(filename=log_file_name, filemode="w", level=logging.INFO)
         self.counter = 0
 
     def generate_row(self):
@@ -54,13 +69,18 @@ class DataRowGenerator:
         attachment_stem_idx = np.random.choice(number_of_stems)
 
         row = get_data_row(
-            self.reference_molMDP, attachment_stem_idx, self.num_conf,
-            self.max_iters, self.random_seed
-            )
+            self.reference_molMDP,
+            attachment_stem_idx,
+            self.child_block_energies_dict,
+            self.max_iters,
+            self.random_seed,
+        )
 
         byte_row = process_row_for_writing_to_feather(row)
         t2 = time.time()
-        self.logger.info(f"     -> Finished row {self.counter}. Execution time: {t2-t1:5.1f} seconds")
+        self.logger.info(
+            f"     -> Finished row {self.counter}. Execution time: {t2-t1:5.1f} seconds"
+        )
         self.counter += 1
 
         return byte_row
