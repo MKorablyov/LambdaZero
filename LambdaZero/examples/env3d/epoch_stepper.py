1d08e97b1a866fc5f26d66b865e003848d76568e
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-08-07 16:33:24 -0400

    Fix MAE bug in EVAL as well.

--- LambdaZero/examples/env3d/epoch_stepper.py
+++ LambdaZero/examples/env3d/epoch_stepper.py
@@ -157,7 +157,7 @@ def eval_epoch(loader, model, device, config):
         ) / max(1, metrics.get("n_angles", 0) + n_angle)
         # we have to do the same for the MAE
         metrics["angle_mae"] = (
-            metrics.get("angle_mae", 0) * metrics.get("n_angles", 0) + angle_mae.item()
+            metrics.get("angle_mae", 0) * metrics.get("n_angles", 0) + angle_mae.item() * n_angle
         ) / max(1, metrics.get("n_angles", 0) + n_angle)
 
     # RMSE and MAE are possibly on gpu. Move to cpu and convert to numpy values (non-tensor)
