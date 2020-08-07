668e88ef11afb35086f4f27c91d45cb6f9affa6c
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-08-07 11:49:32 -0400

    point to correct input

--- LambdaZero/examples/env3d/scripts/run.sh
+++ LambdaZero/examples/env3d/scripts/run.sh
@@ -3,6 +3,6 @@
 
 ROOT_PATH=/Users/bruno/LambdaZero/summaries/env3d/dataset/from_cluster/RUN4/data/
 DATA_FILE_NAME=combined_dataset
-CONFIG=input.json
+CONFIG=example_input.json
 
 python ../train_model.py --root_path $ROOT_PATH --data_file_name $DATA_FILE_NAME --config=$CONFIG
