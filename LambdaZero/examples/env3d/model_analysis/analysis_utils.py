99b507385395050aa63a367b47ff22a26da54409
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-08-10 12:43:12 -0400

    Cleaner code with better test.

--- LambdaZero/examples/env3d/model_analysis/analysis_utils.py
+++ LambdaZero/examples/env3d/model_analysis/analysis_utils.py
@@ -3,36 +3,28 @@ from sklearn.metrics import confusion_matrix
 
 
 def get_truncated_confusion_matrix_and_labels(y_true: np.array, y_predicted: np.array, top_k: int = 3):
+    number_of_values = len(y_true)
+    assert number_of_values == len(y_predicted), "the array size are not the same"
+
     unique_true, true_counts = np.unique(y_true, return_counts=True)
     sorting_indices = np.argsort(true_counts)[::-1]
     unique_true = unique_true[sorting_indices]
 
     top_labels = unique_true[:top_k]
 
-    normalized_y_true = []
-    normalized_y_predicted = []
+    m_true_top_k = np.isin(y_true, top_labels)
+    m_pred_top_k = np.isin(y_predicted, top_labels)
+    m_equal = y_true == y_predicted
 
-    for a, p in zip(y_true, y_predicted):
-        if a in top_labels:
-            a_label = a
-            if p in top_labels:
-                p_label = p
-            else:
-                p_label = 'other'
-        else:
-            if p in top_labels:
-                a_label = 'other'
-                p_label = p
-            else:
-                if a == p:
-                    a_label = 'other match'
-                    p_label = 'other match'
-                else:
-                    p_label = 'other'
-                    a_label = 'other'
+    tmp_true = np.where(m_true_top_k, y_true, "other")
+    tmp_pred = np.where(m_pred_top_k, y_predicted, "other")
+    other_match = np.where(m_equal, "other match", "placeholder")
 
-        normalized_y_true.append(a_label)
-        normalized_y_predicted.append(p_label)
+    mask_true = np.logical_or(m_true_top_k, np.logical_not(m_equal))
+    normalized_y_true = np.where(mask_true, tmp_true, other_match)
+
+    mask_pred = np.logical_or(m_pred_top_k, np.logical_not(m_equal))
+    normalized_y_predicted = np.where(mask_pred, tmp_pred, other_match)
 
     labels = np.concatenate([top_labels, ['other', 'other match']])
 
--- LambdaZero/examples/env3d/tests/test_analysis_utils.py
+++ LambdaZero/examples/env3d/tests/test_analysis_utils.py
@@ -1,22 +1,71 @@
 import numpy as np
+import pytest
+from sklearn.metrics import confusion_matrix
 
 from LambdaZero.examples.env3d.model_analysis.analysis_utils import get_truncated_confusion_matrix_and_labels
 
 
-def test_get_truncated_confusion_matrix_and_labels():
+@pytest.fixture
+def top_k():
+    return 3
 
-    top_k = 3
-    y_true = np.array([0, 0, 0, 1, 1, 5, 5, 5, 5, 5, 5, 5, 27, 11])
-    y_predicted = np.array([1, 0, 0, 1, 1, 5, 5, 3, 9, 0, 1, 5, 27, 12])
 
-    expected_labels = ['5', '0', '1', 'other', 'other match']
+@pytest.fixture
+def y_true_and_y_predicted():
+    np.random.seed(2342)
+    number_of_value = 100
+    probabilities = [0.4, 0.2, 0.1, 0.1, 0.1, 0.05, 0.05]
+    y_true = np.random.choice(range(7), number_of_value, p=probabilities)
+    y_predicted = np.random.choice(range(7), number_of_value, p=probabilities)
+    return y_true, y_predicted
 
-    expected_cm = np.array([[3, 1, 1, 2, 0],
-                            [0, 2, 1, 0, 0],
-                            [0, 0, 2, 0, 0],
-                            [0, 0, 0, 1, 0],
-                            [0, 0, 0, 0, 1]])
 
+@pytest.fixture
+def expected_confusion_matrix_and_labels(y_true_and_y_predicted, top_k):
+    y_true, y_predicted = y_true_and_y_predicted
+
+    unique_true, true_counts = np.unique(y_true, return_counts=True)
+    sorting_indices = np.argsort(true_counts)[::-1]
+    unique_true = unique_true[sorting_indices]
+    top_labels = unique_true[:top_k]
+    expected_labels = np.concatenate([top_labels, ['other', 'other match']])
+
+    normalized_y_true = []
+    normalized_y_predicted = []
+    for a, p in zip(y_true, y_predicted):
+        if a in top_labels:
+            a_label = a
+            if p in top_labels:
+                p_label = p
+            else:
+                p_label = 'other'
+        else:
+            if p in top_labels:
+                a_label = 'other'
+                p_label = p
+            else:
+                if a == p:
+                    a_label = 'other match'
+                    p_label = 'other match'
+                else:
+                    p_label = 'other'
+                    a_label = 'other'
+
+        normalized_y_true.append(a_label)
+        normalized_y_predicted.append(p_label)
+
+    cm = confusion_matrix(normalized_y_true,
+                          normalized_y_predicted,
+                          labels=expected_labels)
+
+    return cm, expected_labels
+
+
+def test_get_truncated_confusion_matrix_and_labels(y_true_and_y_predicted, expected_confusion_matrix_and_labels, top_k):
+
+    expected_cm, expected_labels = expected_confusion_matrix_and_labels
+
+    y_true, y_predicted = y_true_and_y_predicted
     computed_cm, computed_labels = get_truncated_confusion_matrix_and_labels(y_true,
                                                                              y_predicted,
                                                                              top_k)
