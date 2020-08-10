1bd730b068a1009c8f539c628c6d08e417c8db5a
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-08-10 13:24:09 -0400

    Plot confusion matrix and angular error distribution.

---
+++ LambdaZero/examples/env3d/model_analysis/analyse_model.py
@@ -0,0 +1,121 @@
+import logging
+from copy import copy
+from pathlib import Path
+import seaborn as sns
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import ConfusionMatrixDisplay
+from torch.nn import Softmax
+from torch_geometric.data import DataLoader
+
+from LambdaZero.examples.dataset_splitting import RandomDatasetSplitter
+from LambdaZero.examples.env3d.dataset import ENV3D_DATA_PROPERTIES
+from LambdaZero.examples.env3d.dataset.processing import env3d_proc, transform_concatenate_positions_to_node_features
+from LambdaZero.examples.env3d.model_analysis.analysis_utils import get_truncated_confusion_matrix_and_labels, \
+    get_actual_and_predictions
+from LambdaZero.examples.env3d.models.joint_prediction_model import BlockAngleModel
+from LambdaZero.inputs import BrutalDock
+from LambdaZero.utils import get_external_dirs
+
+sns.set(font_scale=1.5)
+
+model_path = Path("/Users/bruno/Desktop/model/Env3dModelTrainer_0_num_angle_prediction_hidden_features=128,num_block_prediction_hidden_features=128,num_edge_network_hidden_feat_2020-08-10_07-55-00rpef7txk//checkpoint_20/model.pth")
+
+model_config = {"num_angle_prediction_hidden_features": 64,
+                "num_block_prediction_hidden_features": 256,
+                "num_edge_network_hidden_features": 32,
+                "num_hidden_features": 128,
+                "number_of_block_classes": 105,
+                "number_of_layers": 2,
+                "set2set_steps": 3,
+                }
+
+dataset_config = {
+    "root": "/Users/bruno/LambdaZero/summaries/env3d/dataset/from_cluster/RUN4/data",
+    "props": ENV3D_DATA_PROPERTIES,
+    "proc_func": env3d_proc,
+    "transform": transform_concatenate_positions_to_node_features,
+    "file_names": ["combined_dataset"],
+}
+
+_, _, summaries_dir = get_external_dirs()
+
+dataset_pickle_path = Path(summaries_dir).joinpath("debug/forecast.pkl")
+dataset_pickle_path.parent.mkdir(exist_ok=True)
+
+
+softmax = Softmax(dim=1)
+
+if __name__ == '__main__':
+    if not dataset_pickle_path.is_file():
+        # generate the forecast if it doesn't exist already
+        # This is DANGEROUS. It is VERY EASY to forget to delete the pickle
+        # to regenerate. Handle with care!
+        model = BlockAngleModel(**model_config)
+        # model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+        model.eval()
+
+        dataset = BrutalDock(**dataset_config)
+        dataset_splitter = RandomDatasetSplitter(train_fraction=0.8,
+                                                 validation_fraction=0.1,
+                                                 random_seed=0)
+
+        training_dataset, _, _ = dataset_splitter.get_split_datasets(dataset)
+
+        batchsize = 256
+        training_dataloader = DataLoader(training_dataset, shuffle=True, batch_size=batchsize)
+
+        logging.info("Creating the forecast pickle.")
+        df = get_actual_and_predictions(model, training_dataloader)
+        df.to_pickle(dataset_pickle_path)
+
+    logging.info("Reading pickle.")
+    df = pd.read_pickle(dataset_pickle_path)
+
+    # Plot confusion matrix
+    cm, display_labels = get_truncated_confusion_matrix_and_labels(y_true=df['actual block'].values,
+                                                                   y_predicted=df['predicted block'].values,
+                                                                   top_k=8)
+
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=display_labels)
+
+    fig = plt.figure(figsize=(12, 8))
+    fig.suptitle("Block index confusion matrix")
+    ax = fig.add_subplot(111)
+    ax.grid(False)
+    disp.plot(include_values=True,
+              cmap='viridis',
+              ax=ax,
+              xticks_rotation='horizontal',
+              values_format=None)
+
+    # Plot angle error
+    angle_df = copy(df[df['actual angle'] >= 0.])
+
+    for type in ['actual', 'predicted']:
+        angle_df[f'{type} cos'] = angle_df[f'{type} angle'].apply(np.cos)
+        angle_df[f'{type} sin'] = angle_df[f'{type} angle'].apply(np.sin)
+
+    actual_vectors = angle_df[['actual cos', 'actual sin']].values
+    predicted_vectors = angle_df[['predicted cos', 'predicted sin']].values
+
+    z = np.array([0., 0., 1.])
+
+    # treat the actual vectors as the x axis in their reference frame. The y axis is then given by
+    y_direction = np.cross(z, actual_vectors)[:, :2]
+
+    x = np.einsum('ij, ij->i', actual_vectors, predicted_vectors)
+    y = np.einsum('ij, ij->i', y_direction, predicted_vectors)
+    angle_error = np.arctan2(y, x)
+
+    fig = plt.figure(figsize=(12, 8))
+    fig.suptitle("Angle error")
+    ax = fig.add_subplot(111)
+    ax.hist(angle_error/np.pi*180., bins=60)
+    ax.set_xlabel("Angle error, in degree")
+    ax.set_ylabel("count")
+    ax.set_xlim([-180., 180.])
+
+    plt.show()
--- LambdaZero/examples/env3d/model_analysis/analysis_utils.py
+++ LambdaZero/examples/env3d/model_analysis/analysis_utils.py
@@ -1,8 +1,14 @@
 import numpy as np
+import pandas as pd
+import torch
 from sklearn.metrics import confusion_matrix
+from torch.nn import Softmax
+from tqdm import tqdm
 
 
-def get_truncated_confusion_matrix_and_labels(y_true: np.array, y_predicted: np.array, top_k: int = 3):
+def get_truncated_confusion_matrix_and_labels(
+    y_true: np.array, y_predicted: np.array, top_k: int = 3
+):
     number_of_values = len(y_true)
     assert number_of_values == len(y_predicted), "the array size are not the same"
 
@@ -26,10 +32,42 @@ def get_truncated_confusion_matrix_and_labels(y_true: np.array, y_predicted: np.
     mask_pred = np.logical_or(m_pred_top_k, np.logical_not(m_equal))
     normalized_y_predicted = np.where(mask_pred, tmp_pred, other_match)
 
-    labels = np.concatenate([top_labels, ['other', 'other match']])
+    labels = np.concatenate([top_labels, ["other", "other match"]])
 
-    cm = confusion_matrix(normalized_y_true,
-                          normalized_y_predicted,
-                          labels=labels)
+    cm = confusion_matrix(normalized_y_true, normalized_y_predicted, labels=labels)
 
     return cm, labels
+
+
+def get_actual_and_predictions(model, training_dataloader):
+    softmax = Softmax(dim=1)
+    list_df = []
+    with torch.no_grad():
+        for batch in tqdm(training_dataloader):
+            blocks_logits, angle_uv = model(batch)
+
+            predicted_blocks = softmax(blocks_logits).argmax(dim=1).cpu().numpy()
+
+            xy = angle_uv.cpu().numpy()
+            x = xy[:, 0]
+            y = xy[:, 1]
+            predicted_angles = get_predicted_angle(x, y)
+            df = pd.DataFrame(
+                data={
+                    "actual block": batch.attachment_block_class.detach().cpu().numpy(),
+                    "actual angle": batch.attachment_angle.detach().cpu().numpy(),
+                    "predicted block": predicted_blocks,
+                    "predicted angle": predicted_angles,
+                }
+            )
+
+            list_df.append(df)
+
+    df = pd.concat(list_df)
+
+    return df
+
+
+def get_predicted_angle(x: np.array, y: np.array):
+    angles = np.arctan2(y, x)
+    return np.mod(angles, 2 * np.pi)
