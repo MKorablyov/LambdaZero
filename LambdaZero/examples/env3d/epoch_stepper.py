35cc691681a03b3e9af01842329348bfdf095502
Simon Blackburn <simon.blackburn@mila.quebec>
2020-08-13 13:23:31 -0400

    BUG: remove )

--- LambdaZero/examples/env3d/epoch_stepper.py
+++ LambdaZero/examples/env3d/epoch_stepper.py
@@ -162,7 +162,7 @@ def eval_epoch(loader, model, device, config):
             metrics.get("angle_mae", 0) * metrics.get("n_angles", 0) + angle_mae.item() * n_angle
         ) / max(1, metrics.get("n_angles", 0) + n_angle)
 
-        metrics["n_angles"] = metrics.get("n_angles", 0) + n_angle)
+        metrics["n_angles"] = metrics.get("n_angles", 0) + n_angle
 
     # RMSE and MAE are possibly on gpu. Move to cpu and convert to numpy values (non-tensor)
     # also take the squareroot of the MSE to get the actual RMSE
