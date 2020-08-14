714dabd624b1adbb3d1e576d7aa529daf84d422c
Cheng-Hao Liu <chenghao.liu@mail.mcgill.ca>
2020-08-14 00:03:02 -0400

    Update __init__.py

--- LambdaZero/environments/__init__.py
+++ LambdaZero/environments/__init__.py
@@ -6,3 +6,4 @@ from .block_mol_v5 import BlockMolEnv_v5
 from .block_mol_graph_v1 import BlockMolEnvGraph_v1
 from .dummy_nonconvex import DummyNonConvex
 from .reward import PredDockReward, PredDockReward_v2, PredDockReward_v3, QEDReward, MorganDistReward
+from .chemMDP import chemMDP
