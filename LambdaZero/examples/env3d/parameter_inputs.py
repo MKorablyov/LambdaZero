e4c61ca3d98ac80770ae1c5647f6d7ee1b830b49
sblackburn86 <35506852+sblackburn86@users.noreply.github.com>
2020-08-14 09:04:56 -0400

    ENH: add args to decouple weights in the MPNN layers (#117)

--- LambdaZero/examples/env3d/parameter_inputs.py
+++ LambdaZero/examples/env3d/parameter_inputs.py
@@ -31,4 +31,24 @@ def read_configuration_file(json_config_path: str):
     with open(json_config_path, "r") as f:
         input_config = json.load(f)
 
+    # json cannot read bool, so convert from str
+    input_config["model_config"]["decouple_layers"] = str2bool(input_config["model_config"].get("decouple_layers", False))
+
     return input_config
+
+
+def str2bool(input):
+    if type(input) == bool:
+        return input
+    elif type(input) == int:
+        # false if 0, true otherwise
+        return input != 0 
+    elif type(input) == str:
+        if input.lower() in ["true", "t"]:
+            return True
+        elif input.lower() in ["false", "f"]:
+            return False
+        else:
+            raise ValueError(f"{input} is an invalid string for decouple_layers argument")
+    else:
+        raise ValueError(f"{input} is an invalid format for decouple_layers. Should be bool, int or str.")
