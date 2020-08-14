cf652ddd0c51632882ca16e82621652b44d015a5
Bruno Rousseau <bruno.rousseau@mila.quebec>
2020-08-14 10:22:37 -0400

    Update path on Bruno's local machine.

--- LambdaZero/examples/env3d/scripts/run.sh
+++ LambdaZero/examples/env3d/scripts/run.sh
@@ -1,7 +1,7 @@
 # This script shows how to drive the execution of the train_model.py script.
 # To run locally, substitute "ROOT_PATH" with a meaningful path.
 
-ROOT_PATH=/Users/bruno/LambdaZero/summaries/env3d/dataset/from_cluster/RUN4/data/
+ROOT_PATH=/Users/bruno/LambdaZero/summaries/env3d/dataset/from_cluster/RUN5/data/
 DATA_FILE_NAME=combined_dataset
 CONFIG=example_input.json
 
