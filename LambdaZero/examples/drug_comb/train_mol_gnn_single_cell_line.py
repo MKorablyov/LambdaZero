d6f997aeba67d50f1cf25be16216475628dda4f5
Jarrid Rector-Brooks <jrectorb@umich.edu>
2020-08-23 16:28:43 -0400

    minors

--- LambdaZero/examples/drug_comb/train_mol_gnn_single_cell_line.py
+++ LambdaZero/examples/drug_comb/train_mol_gnn_single_cell_line.py
@@ -193,7 +193,7 @@ config = {
         "lr": tune.grid_search([1e-3, 1e-4, 1e-5]),
         "batch_size": tune.grid_search([64, 128, 32]),
         "embed_dim": tune.grid_search([64, 128, 256]),
-        "cell_line_idx": tune.grid_search([53, 105, 34, 92, 27, 10, 64, -2]),
+        "cell_line_idx": tune.grid_search([39, 95, 63, 32, 25, 86, 106, 58, 97, 70]),
         "gcn_dropout_rate": .2,
         "lin_dropout_rate": .4,
         "aggr": tune.grid_search(["concat", "hadamard"]),
@@ -207,7 +207,7 @@ config = {
     "checkpoint_freq": 200,
     "stop": {"training_iteration": 100},
     "checkpoint_at_end": False,
-    "resources_per_trial": {},#"gpu": 1},
+    "resources_per_trial": {"gpu": 1},
     "name": "DrugStructureMPNNSiingleCellLines",
     "asha_metric": "eval_mse",
     "asha_mode": "min",
@@ -222,16 +222,6 @@ if __name__ == "__main__":
     time.sleep(time_to_sleep)
     print("Woke up.. Scheduling")
 
-    asha_scheduler = ASHAScheduler(
-	time_attr='training_iteration',
-	metric=config['asha_metric'],
-	mode=config['asha_mode'],
-	max_t=config['asha_max_t'],
-	grace_period=10,
-	reduction_factor=3,
-	brackets=1
-    )
-
     analysis = tune.run(
         config["trainer"],
         name=config["name"],
@@ -242,6 +232,5 @@ if __name__ == "__main__":
         checkpoint_at_end=config["checkpoint_at_end"],
         local_dir=config["summaries_dir"],
         checkpoint_freq=config["checkpoint_freq"],
-        scheduler=asha_scheduler,
     )
 
