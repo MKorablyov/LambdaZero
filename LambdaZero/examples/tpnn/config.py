c2d9daece7ceda53e5db00ee8b0b9e0e28756b94
L-sky <lapchevsky.k@gmail.com>
2020-08-27 23:04:17 +0300

    tpnn: add configs

---
+++ LambdaZero/examples/tpnn/config.py
@@ -0,0 +1,250 @@
+tpnn_default = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)],
+                                [(16, 0, 0), (16, 1, 0)],
+                                [(16, 0, 0), (16, 1, 0)],
+                                [(16, 0, 0), (16, 1, 0)],
+                                [(16, 0, 0), (16, 1, 0)],
+                                [(16, 0, 0), (16, 1, 0)],
+                                [(16, 0, 0), (16, 1, 0)],
+                                [(16, 0, 0), (16, 1, 0)],
+                                [(64, 0, 0)]],
+            "use_set2set": True
+        }
+    }
+}
+
+
+tpnn_2_64_01_set2set = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)],
+                                [(16, 0, 0), (16, 1, 0)],
+                                [(64, 0, 0)]],
+            "use_set2set": True
+        }
+    }
+}
+
+
+tpnn_2_64_01_avg = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)],
+                                [(16, 0, 0), (16, 1, 0)],
+                                [(64, 0, 0)]],
+            "use_set2set": False
+        }
+    }
+}
+
+
+tpnn_3_64_01_set2set = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)],
+                                [(16, 0, 0), (16, 1, 0)],
+                                [(16, 0, 0), (16, 1, 0)],
+                                [(64, 0, 0)]],
+            "use_set2set": True
+        }
+    }
+}
+
+
+tpnn_3_64_01_avg = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)],
+                                [(16, 0, 0), (16, 1, 0)],
+                                [(16, 0, 0), (16, 1, 0)],
+                                [(64, 0, 0)]],
+            "use_set2set": False
+        }
+    }
+}
+
+
+tpnn_2_128_01_set2set = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)],
+                                [(32, 0, 0), (32, 1, 0)],
+                                [(128, 0, 0)]],
+            "use_set2set": True
+        }
+    }
+}
+
+
+tpnn_2_128_01_avg = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)],
+                                [(32, 0, 0), (32, 1, 0)],
+                                [(128, 0, 0)]],
+            "use_set2set": False
+        }
+    }
+}
+
+
+tpnn_3_128_01_set2set = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)],
+                                [(32, 0, 0), (32, 1, 0)],
+                                [(32, 0, 0), (32, 1, 0)],
+                                [(128, 0, 0)]],
+            "use_set2set": True
+        }
+    }
+}
+
+
+tpnn_3_128_01_avg = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)],
+                                [(32, 0, 0), (32, 1, 0)],
+                                [(32, 0, 0), (32, 1, 0)],
+                                [(128, 0, 0)]],
+            "use_set2set": False
+        }
+    }
+}
+
+
+tpnn_2_64_012_set2set = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)],
+                                [(16, 0, 0), (11, 1, 0), (3, 2, 0)],
+                                [(64, 0, 0)]],
+            "use_set2set": True
+        }
+    }
+}
+
+
+tpnn_2_64_012_avg = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)],
+                                [(16, 0, 0), (11, 1, 0), (3, 2, 0)],
+                                [(64, 0, 0)]],
+            "use_set2set": False
+        }
+    }
+}
+
+
+tpnn_3_64_012_set2set = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)],
+                                [(16, 0, 0), (11, 1, 0), (3, 2, 0)],
+                                [(16, 0, 0), (11, 1, 0), (3, 2, 0)],
+                                [(64, 0, 0)]],
+            "use_set2set": True
+        }
+    }
+}
+
+
+tpnn_3_64_012_avg = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)],
+                                [(16, 0, 0), (11, 1, 0), (3, 2, 0)],
+                                [(16, 0, 0), (11, 1, 0), (3, 2, 0)],
+                                [(64, 0, 0)]],
+            "use_set2set": False
+        }
+    }
+}
+
+
+tpnn_2_128_012_set2set = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)],
+                                [(32, 0, 0), (22, 1, 0), (6, 2, 0)],
+                                [(128, 0, 0)]],
+            "use_set2set": True
+        }
+    }
+}
+
+
+tpnn_2_128_012_avg = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)],
+                                [(32, 0, 0), (22, 1, 0), (6, 2, 0)],
+                                [(128, 0, 0)]],
+            "use_set2set": False
+        }
+    }
+}
+
+
+tpnn_3_128_012_set2set = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)],
+                                [(32, 0, 0), (22, 1, 0), (6, 2, 0)],
+                                [(32, 0, 0), (22, 1, 0), (6, 2, 0)],
+                                [(128, 0, 0)]],
+            "use_set2set": True
+        }
+    }
+}
+
+
+tpnn_3_128_012_avg = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)],
+                                [(32, 0, 0), (22, 1, 0), (6, 2, 0)],
+                                [(32, 0, 0), (22, 1, 0), (6, 2, 0)],
+                                [(128, 0, 0)]],
+            "use_set2set": False
+        }
+    }
+}
