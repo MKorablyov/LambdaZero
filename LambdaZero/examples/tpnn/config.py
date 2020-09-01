130486f42162b9fb8f849e57376938989396c93f
L-sky <lapchevsky.k@gmail.com>
2020-09-01 23:48:14 +0300

    tpnn configs: deeper nets

--- LambdaZero/examples/tpnn/config.py
+++ LambdaZero/examples/tpnn/config.py
@@ -1,21 +1,110 @@
-tpnn_default = {
+tpnn_4_64_0_set2set = {
     "trainer_config":
     {
         "model_config":
         {
-            "representations": [[(23, 0, 0)],
-                                [(16, 0, 0), (16, 1, 0)],
-                                [(16, 0, 0), (16, 1, 0)],
-                                [(16, 0, 0), (16, 1, 0)],
-                                [(16, 0, 0), (16, 1, 0)],
-                                [(16, 0, 0), (16, 1, 0)],
-                                [(16, 0, 0), (16, 1, 0)],
-                                [(16, 0, 0), (16, 1, 0)],
-                                [(64, 0, 0)]],
+            "representations": [[(23, 0, 0)]] + [[(64, 0, 0)]] * 3 + [[(64, 0, 0)]],
             "use_set2set": True
         }
     }
 }
+
+
+tpnn_8_64_0_set2set = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)]] + [[(64, 0, 0)]] * 7 + [[(64, 0, 0)]],
+            "use_set2set": True
+        }
+    }
+}
+
+
+tpnn_16_64_0_set2set = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)]] + [[(64, 0, 0)]] * 15 + [[(64, 0, 0)]],
+            "use_set2set": True
+        }
+    }
+}
+
+
+tpnn_4_64_01_set2set = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)]] + [[(16, 0, 0), (16, 1, 0)]] * 3 + [[(64, 0, 0)]],
+            "use_set2set": True
+        }
+    }
+}
+
+
+tpnn_8_64_01_set2set = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)]] + [[(16, 0, 0), (16, 1, 0)]] * 7 + [[(64, 0, 0)]],
+            "use_set2set": True
+        }
+    }
+}
+
+
+tpnn_16_64_01_set2set = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)]] + [[(16, 0, 0), (16, 1, 0)]] * 15 + [[(64, 0, 0)]],
+            "use_set2set": True
+        }
+    }
+}
+
+
+tpnn_4_64_012_set2set = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)]] + [[(16, 0, 0), (11, 1, 0), (3, 2, 0)]] * 3 + [[(64, 0, 0)]],
+            "use_set2set": True
+        }
+    }
+}
+
+
+tpnn_8_64_012_set2set = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)]] + [[(16, 0, 0), (11, 1, 0), (3, 2, 0)]] * 7 + [[(64, 0, 0)]],
+            "use_set2set": True
+        }
+    }
+}
+
+
+tpnn_16_64_012_set2set = {
+    "trainer_config":
+    {
+        "model_config":
+        {
+            "representations": [[(23, 0, 0)]] + [[(16, 0, 0), (11, 1, 0), (3, 2, 0)]] * 15 + [[(64, 0, 0)]],
+            "use_set2set": True
+        }
+    }
+}
+
 
 
 tpnn_2_64_0_set2set = {
