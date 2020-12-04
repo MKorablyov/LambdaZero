5e692ec03fd3e3102eb7d44561d2e620de023edd
Kostiantyn Lapchevskyi <lapchevsky.k@gmail.com>
2020-12-04 22:28:28 +0200

    Update to accomodate DockVina

--- misc/sample_job.sh
+++ misc/sample_job.sh
@@ -9,8 +9,9 @@
 
 module load python/3.6 cuda/10.1 cudnn/7.6.5
 module load nixpkgs/16.09  
-module load gcc/8.3.0 
+module load gcc/7.3.0 
 module load rdkit/2019.03.4
+module load openbabel/2.4.1
 export PATH=$CUDA_PATH/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_PATH/lib64
 export LIBRARY_PATH=$LIBRARY_PATH:$CUDA_PATH/lib64
