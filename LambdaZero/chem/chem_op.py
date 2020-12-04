e1604da1a50efb4f45161b3b71588bcc0cad8f09
L-sky <lapchevsky.k@gmail.com>
2020-12-04 20:44:20 +0200

    formatting

--- LambdaZero/chem/chem_op.py
+++ LambdaZero/chem/chem_op.py
@@ -29,17 +29,17 @@ import torch
 from torch_geometric.data import (Data)
 from torch_sparse import coalesce
 
-atomic_numbers = {"H":1,"He":2,"Li":3,"Be":4,"B":5,"C":6,"N":7,"O":8,"F":9,"Ne":10,"Na":11,"Mg":12,"Al":13,"Si":14,
-                  "P":15, "S":16,"Cl":17,"Ar":18,"K":19,"Ca":20,"Sc":21,"Ti":22,"V":23,"Cr":24,"Mn":25,"Fe":26,"Co":27,
-                  "Ni":28,"Cu":29,"Zn":30,"Ga":31,"Ge":32,"As":33,"Se":34,"Br":35,"Kr":36,"Rb":37,"Sr":38,"Y":39,
-                  "Zr":40, "Nb":41,"Mo":42,"Tc":43,"Ru":44,"Rh":45,"Pd":46,"Ag":47,"Cd":48,"In":49,"Sn":50,"Sb":51,
-                  "Te":52, "I":53,"Xe":54,"Cs":55,"Ba":56}
+atomic_numbers = {"H": 1, "He": 2, "Li": 3, "Be": 4, "B": 5, "C": 6, "N": 7, "O": 8, "F": 9, "Ne": 10, "Na": 11, "Mg": 12, "Al": 13, "Si": 14,
+                  "P": 15, "S": 16, "Cl": 17, "Ar": 18, "K": 19, "Ca": 20, "Sc": 21, "Ti": 22, "V": 23, "Cr": 24, "Mn": 25, "Fe": 26, "Co": 27,
+                  "Ni": 28, "Cu": 29, "Zn": 30, "Ga": 31, "Ge": 32, "As": 33, "Se": 34, "Br": 35, "Kr": 36, "Rb": 37, "Sr": 38, "Y": 39,
+                  "Zr": 40, "Nb": 41, "Mo": 42, "Tc": 43, "Ru": 44, "Rh": 45, "Pd": 46, "Ag": 47, "Cd": 48, "In": 49, "Sn": 50, "Sb": 51,
+                  "Te": 52, "I": 53, "Xe": 54, "Cs": 55, "Ba": 56}
 
 
 def compute_isometry(mol):
     ":return [num_atoms] isometric group "
     isom_groups = list(Chem.rdmolfiles.CanonicalRankAtoms(mol, breakTies=False))
-    return np.asarray(isom_groups,dtype=np.int32)
+    return np.asarray(isom_groups, dtype=np.int32)
 
 
 def find_rota_murcko_bonds(mol, sanitize=False):
@@ -72,8 +72,8 @@ def find_rota_murcko_bonds(mol, sanitize=False):
             rg_bonds = np.asarray(rg_bonds)
             # remove bonds that lead to dummy atoms added by BreakBRICSBonds
             dummy_atoms = np.asarray(r_atmids[rgroup_num]) >= mol.GetNumAtoms()
-            dummy_bonds = dummy_atoms[rg_bonds.reshape([-1])].reshape([-1,2])
-            dummy_bonds = np.logical_or(dummy_bonds[:,0],dummy_bonds[:,1])
+            dummy_bonds = dummy_atoms[rg_bonds.reshape([-1])].reshape([-1, 2])
+            dummy_bonds = np.logical_or(dummy_bonds[:, 0], dummy_bonds[:, 1])
             rg_bonds = rg_bonds[np.logical_not(dummy_bonds)]
             # filter out only the ones that connect core with something outside of the core
             mcore_atmid = np.asarray(r_groups[rgroup_num].GetSubstructMatch(m_core))
@@ -86,7 +86,7 @@ def find_rota_murcko_bonds(mol, sanitize=False):
 
     rota_bonds = np.reshape(np.asarray(rota_bonds, dtype=np.int64), [-1, 2])
     murcko_bonds = np.reshape(np.asarray(murcko_bonds, dtype=np.int64), [-1, 2])
-    return rota_bonds,murcko_bonds
+    return rota_bonds, murcko_bonds
 
 
 def break_on_bonds(mol, jun_bonds, frags_generic):
@@ -102,7 +102,7 @@ def break_on_bonds(mol, jun_bonds, frags_generic):
     # break bonds of the molecule; get fragments
     Chem.Kekulize(mol, clearAromaticFlags=False)
     emol = Chem.EditableMol(mol)
-    [emol.RemoveBond(int(jun_bonds[i,0]), int(jun_bonds[i,1])) for i in range(n_junct)]
+    [emol.RemoveBond(int(jun_bonds[i, 0]), int(jun_bonds[i, 1])) for i in range(n_junct)]
     frags = list(Chem.GetMolFrags(emol.GetMol(), asMols=True, sanitizeFrags=True))
 
     if frags_generic:
@@ -115,7 +115,7 @@ def break_on_bonds(mol, jun_bonds, frags_generic):
                      for i in range(n_junct + 1)]
     mol_canonord = list(np.asarray(frag) for frag in Chem.GetMolFrags(emol.GetMol()))
     mol_canonord = [mol_canonord[i][np.asarray(frag_canonord[i])] for i in range(n_junct+1)]
-    mol_canonord = np.concatenate(mol_canonord,0)
+    mol_canonord = np.concatenate(mol_canonord, 0)
     frags = [Chem.RenumberAtoms(frags[i], frag_canonord[i]) for i in range(n_junct + 1)]
     mol = Chem.RenumberAtoms(mol, [int(idx) for idx in mol_canonord])
     frag_startidx = np.concatenate([[0], np.cumsum([frag.GetNumAtoms() for frag in frags])], 0)[:-1]
@@ -128,10 +128,10 @@ def break_on_bonds(mol, jun_bonds, frags_generic):
         frag_elem.append([frag_atoms[i][j].GetSymbol() for j in range(frag_natm)])
         frag_coord.append(np.asarray([frags[i].GetConformer(0).GetAtomPosition(j) for j in range(frag_natm)]))
     # find junction/fragment bonds for each of the groups
-    bonds = np.asarray([(b.GetBeginAtomIdx(), b.GetEndAtomIdx()) for b in list(mol.GetBonds())]).reshape([-1,2])
+    bonds = np.asarray([(b.GetBeginAtomIdx(), b.GetEndAtomIdx()) for b in list(mol.GetBonds())]).reshape([-1, 2])
     bond_frags = (np.searchsorted(frag_startidx, bonds, side='right') - 1)
     bond_atms = bonds - frag_startidx[bond_frags]
-    bonds = np.concatenate([bond_frags,bond_atms],1)
+    bonds = np.concatenate([bond_frags, bond_atms], 1)
     is_junction = [bond_frags[:, 0] != bond_frags[:, 1]][0]
     jun_bonds = bonds[is_junction]
     assert jun_bonds.shape[0] == n_junct, "internal error in fragmentation"
@@ -163,11 +163,11 @@ def mol_from_frag(jun_bonds, frags=None, frag_smis=None, coord=None, optimize=Fa
     if jun_bonds.size == 0:
         mol_bonds = []
     else:
-        mol_bonds = frag_startidx[jun_bonds[:,0:2]] + jun_bonds[:,2:4]
+        mol_bonds = frag_startidx[jun_bonds[:, 0:2]] + jun_bonds[:, 2:4]
 
     emol = Chem.EditableMol(mol)
 
-    [emol.AddBond(int(bond[0]),int(bond[1]), Chem.BondType.SINGLE) for bond in mol_bonds]
+    [emol.AddBond(int(bond[0]), int(bond[1]), Chem.BondType.SINGLE) for bond in mol_bonds]
     mol = emol.GetMol()
     atoms = list(mol.GetAtoms())
 
@@ -175,7 +175,7 @@ def mol_from_frag(jun_bonds, frags=None, frag_smis=None, coord=None, optimize=Fa
         nh = atom.GetNumExplicitHs()
         if nh > 0: atom.SetNumExplicitHs(nh-1)
 
-    [(_pop_H(atoms[bond[0]]),_pop_H(atoms[bond[1]])) for bond in mol_bonds]
+    [(_pop_H(atoms[bond[0]]), _pop_H(atoms[bond[1]])) for bond in mol_bonds]
     #print([(atom.GetNumImplicitHs(), atom.GetNumExplicitHs(),i) for i,atom in enumerate(mol.GetAtoms())])
     Chem.SanitizeMol(mol)
     # create and optimize 3D structure
@@ -185,7 +185,7 @@ def mol_from_frag(jun_bonds, frags=None, frag_smis=None, coord=None, optimize=Fa
         AllChem.EmbedMolecule(mol)
         AllChem.MMFFOptimizeMolecule(mol)
         Chem.RemoveHs(mol)
-    return mol,mol_bonds
+    return mol, mol_bonds
 
 
 def fragment_molecule(mol, frags_generic, decomposition):
@@ -211,7 +211,7 @@ def fragment_molecule(mol, frags_generic, decomposition):
     return frag_elem, frag_coord, frag_bonds, jun_bonds, frag_names
 
 
-def draw_frag(frag_name,r_idx,out_path,out_file=None):
+def draw_frag(frag_name, r_idx, out_path, out_file=None):
     "0th r_idx should be the actual stem of the group"
     # fixme - this function should be generic
     # create a molecule object
@@ -234,13 +234,13 @@ def draw_frag(frag_name,r_idx,out_path,out_file=None):
     for i in range(frag_proto.GetNumAtoms()):
         opts.atomLabels[i] = frag_proto.GetAtomWithIdx(i).GetSymbol() + str(i)
     Chem.rdDepictor.Compute2DCoords(frag_proto)
-    drawer.DrawMolecule(frag_proto,highlightAtoms=[int(r) for r in r_idx])
+    drawer.DrawMolecule(frag_proto, highlightAtoms=[int(r) for r in r_idx])
     drawer.FinishDrawing()
     svg = drawer.GetDrawingText()
     svg.replace('svg:', '')
 
     if out_file is None:
-        out_path = os.path.join(out_path,fragsp_name + ".svg")
+        out_path = os.path.join(out_path, fragsp_name + ".svg")
     else:
         out_path = os.path.join(out_path, out_file + ".svg")
     svg_file = open(out_path, "w")
@@ -250,7 +250,7 @@ def draw_frag(frag_name,r_idx,out_path,out_file=None):
     return
 
 
-def build_mol(smiles=None,num_conf=1, minimize=False, noh=True,charges=True):
+def build_mol(smiles=None, num_conf=1, minimize=False, noh=True, charges=True):
     # todo: things other than SMILES
     # fixme: return numpy array and not list
     # todo: select min energy?
@@ -271,7 +271,7 @@ def build_mol(smiles=None,num_conf=1, minimize=False, noh=True,charges=True):
     # get elem, get coord
     elem = [int(atom.GetAtomicNum()) for atom in mol.GetAtoms()]
     coord = [np.asarray([mol.GetConformer(j).GetAtomPosition(i) for i in range(len(elem))]) for j in range(num_conf)]
-    coord = np.asarray(np.stack(coord,axis=0),dtype=np.float32).tolist()
+    coord = np.asarray(np.stack(coord, axis=0), dtype=np.float32).tolist()
     return mol, elem, coord
 
 
@@ -280,15 +280,15 @@ def _gen_mol2(smi, mol_name, outpath, chimera_bin, charge_method, num_conf):
     mol = Chem.MolFromSmiles(smi)
     Chem.SanitizeMol(mol)
     mol_h = Chem.AddHs(mol)
-    AllChem.EmbedMultipleConfs(mol_h,numConfs=num_conf)
-    [AllChem.MMFFOptimizeMolecule(mol_h,confId=i) for i in range(num_conf)]
+    AllChem.EmbedMultipleConfs(mol_h, numConfs=num_conf)
+    [AllChem.MMFFOptimizeMolecule(mol_h, confId=i) for i in range(num_conf)]
     mp = AllChem.MMFFGetMoleculeProperties(mol_h, mmffVariant='MMFF94')
     # choose minimum energy conformer
     mi = np.argmin([AllChem.MMFFGetMoleculeForceField(mol_h, mp, confId=i).CalcEnergy() for i in range(num_conf)])
     mol = Chem.RemoveHs(mol_h)
     # save file in .mol format
-    mol_file = os.path.join(outpath,mol_name + ".mol")
-    print(Chem.MolToMolBlock(mol,confId=int(mi)),file=open(mol_file,'w+'))
+    mol_file = os.path.join(outpath, mol_name + ".mol")
+    print(Chem.MolToMolBlock(mol, confId=int(mi)), file=open(mol_file, 'w+'))
     # add hydrogens and compute gasteiger charges in Chimera
     mol2_file = os.path.join(outpath, mol_name + ".mol2")
     _add_hydrogens_and_compute_gasteiger_charges_with_chimera(mol_file, charge_method, chimera_bin, mol2_file)
@@ -329,10 +329,10 @@ class Dock_smi:
         self.dock6_bin = os.path.join(dock6_dir, "bin/dock6")
         if not trustme:
             assert os.path.exists(self.dock6_bin), "can't find dock6 bin " + self.dock6_bin
-        self.vdw_defn_file = os.path.join(dock6_dir,vdw_defn_file)
+        self.vdw_defn_file = os.path.join(dock6_dir, vdw_defn_file)
         if not trustme:
             assert os.path.exists(self.vdw_defn_file), "can't find vdw_def_file " + self.vdw_defn_file
-        self.flex_defn_file = os.path.join(dock6_dir,flex_defn_file)
+        self.flex_defn_file = os.path.join(dock6_dir, flex_defn_file)
         if not trustme:
             assert os.path.exists(self.flex_defn_file), "can't find flex_dfn_file " + self.flex_defn_file
         self.flex_drive_file = os.path.join(dock6_dir, flex_drive_file)
@@ -340,10 +340,10 @@ class Dock_smi:
             assert os.path.exists(self.flex_drive_file), "can't find dock6 bin " + self.flex_drive_file
         self.cleanup = cleanup
         # docking files
-        self.rec_site_file = os.path.join(docksetup_dir,rec_site_file)
+        self.rec_site_file = os.path.join(docksetup_dir, rec_site_file)
         if not trustme:
             assert os.path.exists(self.rec_site_file), "can't find rec site file " + self.rec_site_file
-        self.grid_prefix = os.path.join(docksetup_dir,grid_prefix)
+        self.grid_prefix = os.path.join(docksetup_dir, grid_prefix)
         self.dock_in_template = os.path.join(docksetup_dir, dock_in_template)
         if not trustme:
             assert os.path.exists(self.dock_in_template), "can't find rec site file " + self.dock_in_template
@@ -382,7 +382,7 @@ class Dock_smi:
         with open(dock_pars_file, 'r') as textfile: dock_pars_ = textfile.read()
         dock_pars = dock_pars_.format(mol2file, self.rec_site_file, self.grid_prefix,
                                       self.vdw_defn_file, self.flex_defn_file, self.flex_drive_file,
-                                      os.path.join(outpath,mol_name))
+                                      os.path.join(outpath, mol_name))
         dock_pars_file = os.path.join(outpath, mol_name + "_dockin")
         print(dock_pars, file=open(dock_pars_file, "w+"), end='')
 
@@ -432,7 +432,8 @@ def get_fp(mol, fp_length, fp_radiis, from_atoms=None):
   Returns:
     np.array. shape = [hparams.fingerprint_length]. The Morgan fingerprint.
     """
-    if mol is None: return np.zeros((fp_length * len(fp_radiis),),dtype=np.float32)
+    if mol is None:
+        return np.zeros((fp_length * len(fp_radiis),), dtype=np.float32)
     if from_atoms is not None:
         from_atoms = [int(idx) for idx in from_atoms]
 
@@ -448,8 +449,9 @@ def get_fp(mol, fp_length, fp_radiis, from_atoms=None):
         # np.asarray takes ~ 4.69 ms
         DataStructs.ConvertToNumpyArray(fp, arr)
         fps_.append(arr)
-    fps = np.asarray(np.concatenate(fps_, axis=0),dtype=np.float32)
+    fps = np.asarray(np.concatenate(fps_, axis=0), dtype=np.float32)
     return fps
+
 
 class FPEmbedding_v2:
     def __init__(self, mol_fp_len, mol_fp_radiis, stem_fp_len, stem_fp_radiis):
@@ -470,10 +472,14 @@ class FPEmbedding_v2:
                      get_fp(mol, self.stem_fp_len, self.stem_fp_radiis, [idx[1]]))/2.
                      for idx in molecule.jbond_atmidxs]
 
-        if len(stem_fps) > 0: stem_fps = np.stack(stem_fps, 0)
-        else: stem_fps = np.empty(shape=[0, self.stem_fp_len * len(self.stem_fp_radiis)],dtype=np.float32)
-        if len(jbond_fps) > 0: jbond_fps = np.stack(jbond_fps, 0)
-        else: jbond_fps = np.empty(shape=[0, self.stem_fp_len * len(self.stem_fp_radiis)],dtype=np.float32)
+        if len(stem_fps) > 0:
+            stem_fps = np.stack(stem_fps, 0)
+        else:
+            stem_fps = np.empty(shape=[0, self.stem_fp_len * len(self.stem_fp_radiis)], dtype=np.float32)
+        if len(jbond_fps) > 0:
+            jbond_fps = np.stack(jbond_fps, 0)
+        else:
+            jbond_fps = np.empty(shape=[0, self.stem_fp_len * len(self.stem_fp_radiis)], dtype=np.float32)
         return mol_fp, stem_fps, jbond_fps
 
 
@@ -498,15 +504,16 @@ class FPEmbedding_v2:
 #   return pd.DataFrame({"fingerprint":[arr]})
 
 
-def onehot(arr,num_classes,dtype=np.int):
-    arr = np.asarray(arr,dtype=np.int)
+def onehot(arr, num_classes, dtype=np.int):
+    arr = np.asarray(arr, dtype=np.int)
     assert len(arr.shape) ==1, "dims other than 1 not implemented"
-    onehot_arr = np.zeros(arr.shape + (num_classes,),dtype=dtype)
+    onehot_arr = np.zeros(arr.shape + (num_classes,), dtype=dtype)
     onehot_arr[np.arange(arr.shape[0]), arr] = 1
     return onehot_arr
 
 
 _mpnn_feat_cache = [None]
+
 
 def mpnn_feat(mol, ifcoord=True, panda_fmt=False, one_hot_atom=False, donor_features=True):
     atomtypes = {'H': 0, 'C': 1, 'N': 2, 'O': 3, 'F': 4}
@@ -524,7 +531,7 @@ def mpnn_feat(mol, ifcoord=True, panda_fmt=False, one_hot_atom=False, donor_feat
     atmfeat = np.zeros((natm, nfeat))
 
     # featurize
-    for i,atom in enumerate(mol.GetAtoms()):
+    for i, atom in enumerate(mol.GetAtoms()):
         type_idx = atomtypes.get(atom.GetSymbol(), 5)
         atmfeat[i, type_idx] = 1
         if one_hot_atom:
@@ -564,11 +571,11 @@ def mpnn_feat(mol, ifcoord=True, panda_fmt=False, one_hot_atom=False, donor_feat
     # get bonds and bond features
     bond = np.asarray([[bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()] for bond in mol.GetBonds()])
     bondfeat = [bondtypes[bond.GetBondType()] for bond in mol.GetBonds()]
-    bondfeat = onehot(bondfeat,num_classes=len(bondtypes))
+    bondfeat = onehot(bondfeat, num_classes=len(bondtypes))
 
     # convert atmfeat to pandas
     if panda_fmt:
-        atmfeat_pd = pd.DataFrame(index=range(natm),columns=[
+        atmfeat_pd = pd.DataFrame(index=range(natm), columns=[
             "type_idx", "atomic_number", "acceptor", "donor", "aromatic",
             "sp", "sp2", "sp3", "num_hs"])
         atmfeat_pd['type_idx'] = atmfeat[:, :ntypes+1]
@@ -583,26 +590,28 @@ def mpnn_feat(mol, ifcoord=True, panda_fmt=False, one_hot_atom=False, donor_feat
         atmfeat = atmfeat_pd
     return atmfeat, coord, bond, bondfeat
 
+
 def mol_to_graph_backend(atmfeat, coord, bond, bondfeat, props={}, data_cls=Data):
     "convert to PyTorch geometric module"
     natm = atmfeat.shape[0]
     # transform to torch_geometric bond format; send edges both ways; sort bonds
     atmfeat = torch.tensor(atmfeat, dtype=torch.float32)
     if bond.shape[0] > 0:
-        edge_index = torch.tensor(np.concatenate([bond.T, np.flipud(bond.T)],axis=1),dtype=torch.int64)
-        edge_attr = torch.tensor(np.concatenate([bondfeat,bondfeat], axis=0),dtype=torch.float32)
+        edge_index = torch.tensor(np.concatenate([bond.T, np.flipud(bond.T)], axis=1), dtype=torch.int64)
+        edge_attr = torch.tensor(np.concatenate([bondfeat, bondfeat], axis=0), dtype=torch.float32)
         edge_index, edge_attr = coalesce(edge_index, edge_attr, natm, natm)
     else:
-        edge_index = torch.zeros((0,2), dtype=torch.int64)
+        edge_index = torch.zeros((0, 2), dtype=torch.int64)
         edge_attr = torch.tensor(bondfeat, dtype=torch.float32)
 
     # make torch data
     if coord is not None:
-        coord = torch.tensor(coord,dtype=torch.float32)
+        coord = torch.tensor(coord, dtype=torch.float32)
         data = data_cls(x=atmfeat, pos=coord, edge_index=edge_index, edge_attr=edge_attr, **props)
     else:
         data = data_cls(x=atmfeat, edge_index=edge_index, edge_attr=edge_attr, **props)
     return data
+
 
 def mol_to_graph(smiles, num_conf=1, noh=True, feat="mpnn", dockscore=None, gridscore=None, klabel=None):
     " mol to graph convertor "
