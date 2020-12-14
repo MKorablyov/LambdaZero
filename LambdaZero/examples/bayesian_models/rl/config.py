2937ee96f15224c7a0e00f4bbd13920f010c69e7
Leo Feng <leofeng1999@gmail.com>
2020-12-14 10:35:32 -0500

    configs for dqn bayesian reward

--- LambdaZero/examples/bayesian_models/rl/config.py
+++ LambdaZero/examples/bayesian_models/rl/config.py
@@ -753,3 +753,96 @@ ppo_bayes_reward_019 = {
     },
     "use_dock": True
 }
+
+
+########### DQN CONFIGS
+
+dqn_bayes_reward_001 = {
+    "rllib_config":{
+        "env": BlockMolEnvGraph_v1,
+        "env_config": {
+            "allow_removal": True,
+            "reward": PredDockBayesianReward_v1,
+            "reward_config": {
+                "synth_config": synth_config,
+                "binding_model": binding_model,
+                "delta": True,
+                "dense_rewards": True
+            },
+        },
+        "model": {
+            "custom_model": "GraphMolDQN_thv1",
+            "custom_options":{"dqn_rew_type": "DQN",
+                              "eps_anneal_timelength": int(5e4)} # does a **kw to __init__,
+        },
+        "lr": 5e-5,
+        # "entropy_coeff": 1e-4,
+        # "entropy_coeff_schedule": [(0, 1e-4), (10000, 5e-5), (100000, 1e-5), (1000000, 1e-6)],
+        "framework": "torch",
+    },
+    "reward_learner_config": {
+        "num_mol_retrain": 1000,
+        "kappa": 5
+    },
+    "use_dock": True
+}
+
+max_dqn_bayes_reward_000 = {
+    "rllib_config":{
+        "env": BlockMolEnvGraph_v1,
+        "env_config": {
+            "allow_removal": True,
+            "reward": PredDockBayesianReward_v1,
+            "reward_config": {
+                "synth_config": synth_config,
+                "binding_model": binding_model,
+                "delta": True,
+                "dense_rewards": True
+            },
+        },
+        "model": {
+            "custom_model": "GraphMolDQN_thv1",
+            "custom_options":{"num_hidden": 64,
+                              "dqn_rew_type": "MaxDQN"} # does a **kw to __init__,
+        },
+        "lr": 5e-5,
+        # "entropy_coeff": 1e-4,
+        # "entropy_coeff_schedule": [(0, 1e-4), (10000, 5e-5), (100000, 1e-5), (1000000, 1e-6)],
+        "framework": "torch",
+    },
+    "reward_learner_config": {
+        "num_mol_retrain": 1000,
+        "kappa": 5
+    },
+    "use_dock": True
+}
+
+max_dqn_bayes_reward_001 = {
+    "rllib_config":{
+        "env": BlockMolEnvGraph_v1,
+        "env_config": {
+            "allow_removal": True,
+            "reward": PredDockBayesianReward_v1,
+            "reward_config": {
+                "synth_config": synth_config,
+                "binding_model": binding_model,
+                "delta": True,
+                "dense_rewards": True
+            },
+        },
+        "model": {
+            "custom_model": "GraphMolDQN_thv1",
+            "custom_options":{"dqn_rew_type": "MaxDQN",
+                              "eps_anneal_timelength": int(5e4)} # does a **kw to __init__,
+        },
+        "lr": 5e-5,
+        # "entropy_coeff": 1e-4,
+        # "entropy_coeff_schedule": [(0, 1e-4), (10000, 5e-5), (100000, 1e-5), (1000000, 1e-6)],
+        "framework": "torch",
+    },
+    "reward_learner_config": {
+        "num_mol_retrain": 1000,
+        "kappa": 5
+    },
+    "use_dock": True
+}
