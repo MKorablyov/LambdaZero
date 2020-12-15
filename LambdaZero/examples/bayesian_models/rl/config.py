6865eae8a07de3c9181872f1c9bbc0cbd8aca09a
Leo Feng <leofeng1999@gmail.com>
2020-12-15 18:53:56 -0500

    add script

--- LambdaZero/examples/bayesian_models/rl/config.py
+++ LambdaZero/examples/bayesian_models/rl/config.py
@@ -754,6 +754,32 @@ ppo_bayes_reward_019 = {
     "use_dock": True
 }
 
+ppo_bayes_reward_020 = {
+    "rllib_config":{
+        "env": BlockMolEnvGraph_v1,
+        "env_config": {
+            "allow_removal": True,
+            "reward": PredDockBayesianReward_v1,
+            "reward_config": {
+                "synth_config": synth_config,
+                "binding_model": binding_model,
+            },
+        },
+        "model": {
+            "custom_model": "GraphMolActorCritic_thv1",
+            "custom_options":{"num_hidden": 64} # does a **kw to __init__
+        },
+        "lr": 5e-5,
+        "entropy_coeff": 1e-3,
+        # "entropy_coeff_schedule": [(0, 1e-4), (10000, 5e-5), (100000, 1e-5), (1000000, 1e-6)],
+        "framework": "torch",
+    },
+    "reward_learner_config": {
+        "num_mol_retrain": 1000,
+        "kappa": 0
+    }
+}
+
 
 ########### DQN CONFIGS
 dqn_bayes_reward_000 = {
