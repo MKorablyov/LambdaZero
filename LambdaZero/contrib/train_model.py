9b8e8baf61a88bc680a2977f90e0975cd6c91761
Maksym Korablyov <mkkr@beluga5.int.ets1.calculquebec.ca>
2021-02-16 23:34:31 -0500

    uncertainty model running

--- LambdaZero/contrib/train_model.py
+++ LambdaZero/contrib/train_model.py
@@ -1,32 +1,65 @@
 from model_with_uncertainty.molecule_models import MolMCDropGNN
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import torch
 
+from torch.utils.data import Dataset
+from LambdaZero.inputs import mol_to_graph
+from rdkit import Chem
 
-def load_dataset():
-    x = np.random.uniform(size=(10000,1024))
-    func = np.random.uniform(size=(1024,1)) / 1024.
-    y = np.matmul(x, func)
-    return x,y
+import os.path as osp
+import torch_geometric.transforms as T
+import LambdaZero.utils
+import LambdaZero.inputs
 
+from LambdaZero.environments.persistent_search.persistent_buffer import BlockMolEnvGraph_v1
+from LambdaZero.environments.reward import PredDockReward_v2
+from LambdaZero.contrib.proxy import ProxyUCB
+from LambdaZero.contrib.reward import ProxyReward,DummyReward
+from LambdaZero.contrib.model_with_uncertainty import MolMCDropGNN
+from LambdaZero.contrib.oracle import DockingOracle
+from LambdaZero.contrib.inputs import load_data_v1
+datasets_dir, programs_dir, summaries_dir = LambdaZero.utils.get_external_dirs()
 
-def standardize_dataset(x, y):
-    x = StandardScaler().fit_transform(x)
-    y = StandardScaler().fit_transform(y)
-    return x, y
+import ray
+ray.init()
 
+def load_data_v1(target, dataset_split_path, dataset, dataset_config):
+    dataset = dataset(**dataset_config)
+    train_idxs, val_idxs, test_idxs = np.load(dataset_split_path, allow_pickle=True)
+    data_list = []
+    y_list = []
+    for graph in dataset:
+        y_list.append(getattr(graph,target))
+        delattr(graph,target)
+        delattr(graph, "pos")
+        delattr(graph, "smiles")
+        graph.x = torch.cat([graph.x, torch.zeros([graph.x.shape[0],2])],dim=1)
+        graph.jbond_atmidx=torch.zeros([0, 2])
+        graph.jbond_preds=torch.zeros([0, 0])
+        graph.stem_atmidx=torch.zeros([0])
+        graph.stem_preds=torch.zeros([0, 0])
+        data_list.append(graph)
 
-def split_dataset(x, y):
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.25, random_state=42)
-    x_train, y_train = torch.tensor(x_train).float(), torch.tensor(y_train).float()
-    x_test, y_test = torch.tensor(x_test).float(), torch.tensor(y_test).float()
-    return x_train, y_train, x_test, y_test
+    train_x = [{"mol_graph":data_list[int(i)]} for i in train_idxs]
+    train_y = [y_list[i] for i in train_idxs]
+    val_x = [{"mol_graph":data_list[int(i)]} for i in val_idxs]
+    val_y = [y_list[i] for i in val_idxs]
+    return train_x, train_y, val_x, val_y
 
-
-x, y = load_dataset()
-print("loaded dataset")
-x, y = standardize_dataset(x, y)
-print("standardized dataset")
-x_train, y_train, x_test, y_test = split_dataset(x, y)
-print("splitted dataset")
+train_x, train_y, val_x, val_y = load_data_v1(
+        'dockscore', 
+        osp.join(datasets_dir, "brutal_dock/seh/raw/split_Zinc20_docked_neg_randperm_3k.npy"), 
+        LambdaZero.inputs.BrutalDock, 
+        {
+            "root": osp.join(datasets_dir, "brutal_dock/seh"),
+            "props": ["dockscore", "smiles"],
+            "transform": T.Compose([LambdaZero.utils.Complete(),
+            LambdaZero.utils.Normalize("dockscore", -8.6, 1.1)]),
+            "file_names": ["Zinc20_docked_neg_randperm_3k"],
+        }
+        )
 
 model = MolMCDropGNN()
-model.fit()
+model.fit(train_x, train_y)
