f9470cad6ccc110b3e71a22b822f03846d5f7461
mkorablyov <mkorablyov@gmail.com>
2021-02-26 23:44:55 -0500

    added retries on ray fails

--- LambdaZero/contrib/train_rlbo.py
+++ LambdaZero/contrib/train_rlbo.py
@@ -28,30 +28,38 @@ if machine == "Ikarus":
 
 
 if __name__ == "__main__":
-    ray.init(object_store_memory=config["object_store_memory"], _memory=config["memory"])
-    ModelCatalog.register_custom_model("GraphMolActorCritic_thv1", GraphMolActorCritic_thv1)
-    # initialize loggers
-    os.environ['WANDB_DIR'] = summaries_dir
-    os.environ["WANDB_MODE"] = "dryrun"
-    remote_logger = RemoteLogger.remote()
-    #time.sleep(10) # this might not be needed, but added due to unsolved wandb init errors
-    wandb_logger = WandbRemoteLoggerCallback(
-        remote_logger=remote_logger,
-        project=config["tune_config"]["config"]["logger_config"]["wandb"]["project"],
-        api_key_file=osp.join(summaries_dir, "wandb_key"))
-    config["tune_config"]['config']['env_config']["reward_config"]["scoreProxy_config"]["logger"] = remote_logger
-    config["tune_config"]['config']['env_config']["reward_config"]["scoreProxy_config"]["acquirer_config"]\
-        ["model_config"]["logger"] = remote_logger
-    config["tune_config"]["loggers"] = DEFAULT_LOGGERS + (wandb_logger,)
+    for i in range(7):
+        try:
+            # Maksym Feb 26
+            # I have been trying to debug the issue of ray sometimes resulting in GPU-OOM
+            # what seems to happen is that ray jobs sometimes fail very soon after the initialization
+            # same exact jobs can run for a while when initialized again. I think the issue is related to how individual
+            # workers are allocated. Yet, I have not been able to entirely debug it
 
-    # initialize scoreProxy which would be shared across many agents
-    scoreProxy = config["tune_config"]['config']['env_config']['reward_config']['scoreProxy'].\
-        options(**config["tune_config"]['config']['env_config']['reward_config']['scoreProxy_options']).\
-        remote(**config["tune_config"]['config']['env_config']['reward_config']['scoreProxy_config'])
+            ray.init(object_store_memory=config["object_store_memory"], _memory=config["memory"])
+            ModelCatalog.register_custom_model("GraphMolActorCritic_thv1", GraphMolActorCritic_thv1)
+            # initialize loggers
+            os.environ['WANDB_DIR'] = summaries_dir
+            os.environ["WANDB_MODE"] = "dryrun"
+            remote_logger = RemoteLogger.remote()
+            # time.sleep(10) # this might not be needed, but added due to unsolved wandb init errors
+            wandb_logger = WandbRemoteLoggerCallback(
+                remote_logger=remote_logger,
+                project=config["tune_config"]["config"]["logger_config"]["wandb"]["project"],
+                api_key_file=osp.join(summaries_dir, "wandb_key"))
+            config["tune_config"]['config']['env_config']["reward_config"]["scoreProxy_config"][
+                "logger"] = remote_logger
+            config["tune_config"]['config']['env_config']["reward_config"]["scoreProxy_config"]["acquirer_config"] \
+                ["model_config"]["logger"] = remote_logger
+            config["tune_config"]["loggers"] = DEFAULT_LOGGERS + (wandb_logger,)
 
-    config["tune_config"]['config']['env_config']['reward_config']['scoreProxy'] = scoreProxy
-
-    #print(ray.get(scoreProxy.get_acquisition_func.remote()))
-
-    # run
-    tune.run(**config["tune_config"], trial_name_creator=TrialNameCreator(config_name))
+            # initialize scoreProxy which would be shared across many agents
+            scoreProxy = config["tune_config"]['config']['env_config']['reward_config']['scoreProxy']. \
+                options(**config["tune_config"]['config']['env_config']['reward_config']['scoreProxy_options']). \
+                remote(**config["tune_config"]['config']['env_config']['reward_config']['scoreProxy_config'])
+            config["tune_config"]['config']['env_config']['reward_config']['scoreProxy'] = scoreProxy
+            # run
+            tune.run(**config["tune_config"], trial_name_creator=TrialNameCreator(config_name))
+        except Exception as e:
+            print(e)
+        ray.shutdown()
