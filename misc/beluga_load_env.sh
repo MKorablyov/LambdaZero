08b311d4eb87f5ee66c3867e6f39e39a98ab2941
MKorablyov <MKorablyov@gmail.com>
2021-04-01 19:30:33 -0400

    Update beluga_load_env.sh

--- misc/beluga_load_env.sh
+++ misc/beluga_load_env.sh
@@ -1,7 +1,7 @@
 # source: docs.mila.quebec
 # case 1: to get an interactive allocation run: salloc --time=1:0:0 --cpus-per-task=4 --gres=gpu:1 --mem=32G --account=rrg-bengioy-ad
 # case 2: to submit a beluga batch job don't run this scrip directly, instead modify and run beluga_gpu.sh or beluga_gpu.sh in this folder
-
+module load StdEnv/2018.3
 module load python/3.6 cuda/10.1 cudnn/7.6.5
 module load nixpkgs/16.09
 module load gcc/7.3.0
