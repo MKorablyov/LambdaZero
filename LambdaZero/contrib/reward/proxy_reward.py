58598c54fb5b484515313bda385f3bc3cd80cbc6
andreicnica <andreic.nica@gmail.com>
2021-05-07 15:37:32 -0400

    Send qed and synth to oracle

--- LambdaZero/contrib/reward/proxy_reward.py
+++ LambdaZero/contrib/reward/proxy_reward.py
@@ -6,9 +6,11 @@ from random import random
 from LambdaZero.contrib.oracle import QEDOracle, SynthOracle
 from LambdaZero.environments.block_mol_v3 import synth_config
 import ray
+from copy import deepcopy
 
 from LambdaZero.inputs.inputs_op import _brutal_dock_proc
 from LambdaZero.contrib.config_acquirer import LOG_CANDIDATE_QED_SCORE, LOG_CANDIDATE_SYNTH_SCORE
+from LambdaZero.contrib.oracle.oracle import DockVina_smi_db
 
 import LambdaZero.contrib.functional
 
@@ -39,6 +41,8 @@ class ProxyReward:
         clip_qed = LambdaZero.contrib.functional.satlins(qed, self.qed_cutoff[0], self.qed_cutoff[1])
         clip_synth = LambdaZero.contrib.functional.satlins(synth_score, self.synth_cutoff[0], self.synth_cutoff[1])
         proxy_dock, actor_info = self.dockProxy_actor([{"smiles": molecule.smiles,
+                                                        "qed": qed,
+                                                        "synth_score": synth_score,
                                                         "mol_graph": molecule.graph,
                                                         "blockidxs": molecule.blockidxs,
                                                         "slices": molecule.slices,
@@ -56,14 +60,14 @@ class ProxyReward:
         info = {
             "molecule_num_blocks": len(molecule.jbond_atmidxs),
             "molecule_num_branches":len(molecule.stems),
-            "molecule_num_atoms":molecule.slices[-1],
+            "molecule_num_atoms": molecule.slices[-1],
             "proxy_cand_dock_mean": cand_dock_mean,
             "proxy_dock": proxy_dock,
             "proxy_dock_mean": actor_info["mean"][0],
             "proxy_dock_var": actor_info["var"][0],
             "synth_score": synth_score, "qed_score":qed,
             "clip_qed": clip_qed, "clip_synth": clip_synth,
-            **actor_info # TODO not sure how much it clogs
+            **actor_info  # TODO not sure how much it clogs
         }
 
         return reward, info
@@ -82,42 +86,5 @@ class ProxyRewardSparse(ProxyReward):
         return reward, info
 
 
-class ProxyRewardOradcle(ProxyReward):
-    def __init__(self, scoreProxy, actor_sync_freq, qed_cutoff, synth_cutoff,
-                 exp_dock, always_discount, synth_options, **kwargs):
-        self.env_name = np.random.uniform()
-        self.qed_cutoff = qed_cutoff
-        self.synth_cutoff = synth_cutoff
-        self.exp_dock = exp_dock
-        self.always_discount = always_discount
-        self.qed_oracle = QEDOracle(num_threads=1)
-        self.synth_oracle = SynthOracle(synth_options, synth_config)
-        self.dockProxy_actor = Actor(scoreProxy, actor_sync_freq)
-
-        # Candidate Molecule hard thresholds (Same in Proxy)
-        # Should be fixed in order to be able to compare runs across different training configs
-        self._log_cand_qed_th = LOG_CANDIDATE_QED_SCORE
-        self._log_cand_synth_th = LOG_CANDIDATE_SYNTH_SCORE
-
-    def __call__(self, molecule, agent_stop, env_stop, num_steps):
-        if agent_stop or env_stop:
-            reward, info = ProxyReward.eval(self, molecule)
-            info["ended_on_env_stop"] = float(env_stop)
-        else:
-            reward, info = 0.0, {}
-        return reward, info
 
 
-
-
-
-class DummyReward:
-    def __init__(self, **kwargs):
-        self.qed_oracle = QEDOracle(num_threads=1)
-
-    def reset(self, previous_reward=0.0):
-        self.previous_reward = 0.0
-        return None
-
-    def __call__(self, molecule, agent_stop, env_stop, num_steps):
-        return float(random()), {"reward": 1.0, "discounted_reward": 1.0, "QED": 1.0, "discount": 1.0}
