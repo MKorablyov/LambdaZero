05744f3b3dc0f801a59333987f0a62e1a80b22d1
L-sky <lapchevsky.k@gmail.com>
2021-06-14 15:49:14 +0300

    DockedDataset: update to match new docking interface; use alias create new processed subfolder, rather than file

--- LambdaZero/datasets/docked.py
+++ LambdaZero/datasets/docked.py
@@ -8,16 +8,25 @@ from tqdm import tqdm
 from LambdaZero.chem import DockVina_smi
 
 
+# TODO: class can be launched with different docker_kwargs, but only one set persists - single raw file - make a warning if newly supplied docker_kwargs mismatch
+#  more less the same mechanism as torch_geometric uses for pre_transform and pre_filter
 class DockedDataset(InMemoryDataset):
-    def __init__(self, root, file_name, alias, transform=None, pre_transform=None, pre_filter=None, docker_kwargs=None):
+    def __init__(self, root, file_name, alias, transform=None, pre_transform=None, pre_filter=None, **docker_kwargs):
         self.file_name_no_ext = file_name.split('.')[0]
         self.alias = alias
+        self.docker_kwargs = docker_kwargs
+
         if pre_transform is None:
-            pre_transform = DockedDataset.get_best_conformation
-        self.docker_kwargs = docker_kwargs if docker_kwargs is not None else {'mode': "all_conf"}
+            pre_transform = DockedDataset.get_best_conformation_minimal
 
         super().__init__(root, transform, pre_transform, pre_filter)
         self.data, self.slices = torch.load(self.processed_paths[0])
+
+    @property
+    def processed_dir(self):
+        # extend default behaviour to go extra directory in depth
+        # this allows to have multiple processed sets for the same raw file
+        return os.path.join(super().processed_dir, self.alias)
 
     @property
     def raw_file_names(self):
@@ -25,9 +34,10 @@ class DockedDataset(InMemoryDataset):
 
     @property
     def processed_file_names(self):
-        return f"{self.file_name_no_ext}_{self.alias}.pth"
+        return f"{self.file_name_no_ext}.pth"
 
     def construct_graphs(self):
+        """Construct graph (Data object) as a collection of provided attributes and results of docking"""
         graphs_list = []
         docker = DockVina_smi(outpath=self.root, **self.docker_kwargs)
         input_csv_file = os.path.join(self.root, f"{self.file_name_no_ext}.csv")
@@ -38,13 +48,11 @@ class DockedDataset(InMemoryDataset):
                 graph = Data(**entry)
                 graph['idx'] = idx
                 # dock molecule and append obtained info
-                mol_name, dockscores, original_pos, docked_pos = docker.dock(entry['smiles'])
+                _, mol_name, dockscores, free_pose, docked_poses = docker.dock(entry['smiles'])
                 graph['mol_name'] = mol_name
                 graph['dockscores'] = torch.from_numpy(dockscores)
-                # flatten dimensions, so that custom collate is not needed
-                # those dimensions can be restored on get, since we can infer dimensions from dockscores and original_pos shapes
-                graph['original_pos'] = torch.from_numpy(original_pos)
-                graph['docked_pos'] = torch.from_numpy(docked_pos)
+                graph['free_pose'] = torch.from_numpy(free_pose)
+                graph['docked_poses'] = torch.from_numpy(docked_poses)
                 graphs_list.append(graph)
             except Exception as e:
                 # Docking can fail for some smiles, whether it does is mostly inconsequential though.
@@ -74,29 +82,31 @@ class DockedDataset(InMemoryDataset):
         torch.save(self.collate(graphs_list), self.processed_paths[0])
 
     @staticmethod
-    def get_best_score_only(graph):
-        graph.dockscore = graph.dockscores[0]
-        graph.docked_pos = None
-        graph.original_pos = None
-        graph.dockscores = None
+    def get_best_score_only_minimal(raw_graph):
+        graph = Data()
+        graph.idx = raw_graph.idx
+        graph.y = raw_graph.dockscores[[0]]  # keep batch dimension, there would be error on collation otherwise
+        graph.smiles = raw_graph.smiles
         return graph
 
     @staticmethod
-    def get_best_conformation(graph):
-        graph.dockscore = graph.dockscores[0]
-        graph.docked_pos = graph.docked_pos[0]
-        graph.pos = graph.original_pos
-        graph.dockscores = None
+    def get_best_conformation_minimal(raw_graph):
+        graph = Data()
+        graph.idx = raw_graph.idx
+        graph.y = raw_graph.dockscores[[0]]  # keep batch dimension
+        graph.pos = raw_graph.free_pose
+        graph.docked_pos = raw_graph.docked_poses[0]
+        graph.smiles = raw_graph.smiles
         return graph
 
     @staticmethod
-    def get_all_conformations(graph):
+    def get_all_docked_conformations_minimal(raw_graph):
         graphs_list = []
-        for dockscore, pos in zip(graph.dockscores, graph.docked_pos):
-            tmp_graph = graph.clone()
-            tmp_graph.pos = pos
-            tmp_graph.docked_pos = pos
-            tmp_graph.dockscore = dockscore
-            tmp_graph.dockscores = None
-            graphs_list.append(tmp_graph)
+        for dockscore, pos in zip(raw_graph.dockscores, raw_graph.docked_poses):
+            graph = Data()
+            graph.idx = raw_graph.idx
+            graph.y = [dockscore]  # restore batch dimension
+            graph.pos = pos
+            graph.smiles = raw_graph.smiles
+            graphs_list.append(graph)
         return graphs_list
