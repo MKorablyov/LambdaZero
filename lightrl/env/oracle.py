fdb9fa748d05f6b6aa53ffcbb34c51fd5ce95a54
andreicnica <andreic.nica@gmail.com>
2021-06-22 10:16:12 -0400

    Dock score remote oracle and info dict transformers

---
+++ lightrl/env/oracle.py
@@ -0,0 +1,159 @@
+import cv2
+import numpy as np
+from multiprocessing import Process, Pipe, Queue
+import multiprocessing
+import copy
+from argparse import Namespace
+
+from LambdaZero.contrib.oracle.oracle import DockVina_smi_db
+from LambdaZero.contrib.config_acquirer import oracle_config
+from LambdaZero.contrib.oracle import DockingOracle
+
+
+class FakeRemoteLog:
+    def __init__(self):
+        self._last_log = None
+        self.log = Namespace(remote=self.remote)
+
+    def remote(self, log):
+        self._last_log = log
+
+
+class DockingEstimatorThread(DockVina_smi_db):
+    """ Docking estimator separate for each env. """
+    def __init__(self, dockVina_config):
+        DockVina_smi_db.__init__(self, **dockVina_config)
+
+    def eval(self, smiles, **kwargs):
+        try:
+            mol_name, dockscore, coord = self.dock(smiles, **kwargs)
+        except Exception as e:
+            dockscore = None
+        return dockscore
+
+
+def compute_dock(conn, conn_send):
+    cfg = copy.deepcopy(oracle_config["dockVina_config"])
+    cfg["query_predocked"] = False
+    dockestm = DockingEstimatorThread(cfg)
+
+    while True:
+        _id, smiles, data = conn.get()
+        ret = dockestm.eval(smiles, mol_data=data)
+        conn_send.put((_id, smiles, ret))
+
+
+class InterogateOracle:
+    """
+        Interogate global thread for looking up predocked scores. (1 db loaded)
+        If not found use local thread to calculate oracle (+update db)
+    """
+
+    def __init__(self, num_workers=4):
+        fake_logger = FakeRemoteLog()
+        oracle_config["logger"] = fake_logger
+        oracle_config["num_threads"] = 0
+        dock_oracle = DockingOracle(**oracle_config)
+
+        self._dock_db = dock_oracle
+        self._dock_db.set_fix_none(False)
+        self._dock_db.set_norm_data(False)
+
+        self.remote_send = remote_send = Queue()
+        self.remote_recv = remote_recv = Queue()
+
+        self.processes = []
+        for _ in range(num_workers):
+            p = Process(
+                target=compute_dock,
+                args=(remote_send, remote_recv))
+            p.daemon = True
+            p.start()
+
+            self.processes.append(p)
+
+        self._oracle_req = dict()
+        self._ready_req = dict()
+        # self.mean = oracle_config["mean"]
+        # self.std = oracle_config["std"]
+        # self.act_y = oracle_config["act_y"]
+
+    def __call__(self, x):
+        _id = np.random.randint(10000)
+
+        # Send only
+        smi_data = [{"smiles": i["smiles"]} for i in x]
+        res, tres = self._dock_db(smi_data, ret_true_dockscore=True)
+
+        for i, (smi, data, dockscore, true_dock) in enumerate(zip(smi_data, x, res, tres)):
+            if dockscore is None and smi["smiles"] not in self._oracle_req:
+                self._oracle_req[smi["smiles"]] = True
+                self.remote_send.put((_id, smi["smiles"], data))
+            elif dockscore is not None:
+                # In case we reached max failed hits on a smile we should put None
+                self._ready_req[smi["smiles"]] = true_dock
+
+    def get_req(self, smiles=None):
+        if smiles is not None:
+            raise NotImplementedError
+
+        # Return all req
+        req = self._ready_req
+        self._ready_req = dict()
+
+        while len(self._oracle_req) > 0:
+            _id, smiles, ret = self.remote_recv.get()
+            self._dock_db.update_predocked(smiles, ret)
+            req[smiles] = ret
+            self._oracle_req.pop(smiles)
+        return req
+
+
+if __name__ == "__main__":
+    import gym
+    from lightrl.env.reward import DummyReward
+    import numpy as np
+
+    oracle = InterogateOracle()
+
+    env = gym.make("BlockMolEnvGraph-v1", config={
+        "reward": DummyReward,
+        "random_steps": 4,
+        "max_steps": 5,
+        "allow_removal": True,
+        "env_seed": 15}, proc_id=0)
+
+    obs = env.reset()
+    act = np.random.choice(np.where(obs["action_mask"])[0])
+    obs, r, done, info = env.step(act)
+    req_dock = [env.unwrapped.molMDP.dump()]
+    oracle(req_dock)
+    oracle(req_dock)
+    oracle(req_dock)
+    req_dock[0]["smiles"] = "sdasdas"
+    oracle(req_dock)
+    oracle(req_dock)
+    oracle(req_dock)
+
+    # pre docked
+    env = gym.make("BlockMolEnvGraph-v1", config={
+        "reward": DummyReward,
+        "random_steps": 3,
+        "max_steps": 1,
+        "allow_removal": True,
+        "env_seed": [13]}, proc_id=0)
+
+    obs = env.reset()
+
+    for i in range(10):
+        act = np.random.choice(np.where(obs["action_mask"])[0])
+        obs, r, done, info = env.step(act)
+
+        req_dock = [env.unwrapped.molMDP.dump()]
+        oracle(req_dock)
+
+        if done:
+            obs = env.reset()
+
+    ret_req = oracle.get_req()
+    print(ret_req)
---
+++ lightrl/env/transforms.py
@@ -0,0 +1,64 @@
+from typing import List, Dict, Tuple
+import numpy as np
+
+from lightrl.env.oracle import InterogateOracle
+from LambdaZero.contrib.functional import elu2, satlins
+
+
+class TransformInfoOracle:
+    def __init__(self):
+        self.oracle = InterogateOracle()
+
+    def __call__(self, infos: List[dict]):
+        _match = dict({x["mol"]["smiles"]: idx for idx, x in enumerate(infos)})
+        self.oracle([x["mol"] for x in infos])
+        ret_scores = self.oracle.get_req()
+        for smi, dockscore in ret_scores.items():
+            infos[_match[smi]]["dockscore"] = dockscore
+        return infos
+
+
+class TransformInfoDiscounted:
+    def __init__(self,
+                 score_key="dockscore", mean=-8.6, std=1.1, act_y=elu2,
+                 qed_cutoff=(0.2, 0.5), synth_cutoff=(0, 4)):
+        self._score_key = score_key
+        self._mean = mean
+        self._std = std
+        self._act_y = act_y
+        self._qed_cut = qed_cutoff
+        self._synth_cut = synth_cutoff
+
+    def __call__(self, infos: List[dict]):
+        sk = self._score_key
+        filter = [True if (sk in x and x[sk] is not None) else False for x in infos]
+        idxs = np.where(filter)[0]
+        scores = [infos[x][sk] for x in idxs]
+        scores = [(self._mean - d) / self._std for d in scores]  # this normalizes and flips dockscore
+        scores = np.array(self._act_y(scores))
+        clip_qed = np.array([
+            satlins(q, self._qed_cut[0], self._qed_cut[1]) for q in [infos[x]["qed"] for x in idxs]
+        ])
+
+        clip_synth = np.array([
+            satlins(q, self._synth_cut[0], self._synth_cut[1]) for q in [infos[x]["synth"] for x in idxs]
+        ])
+
+        scores = scores * clip_synth * clip_qed
+        for idx, info in enumerate(infos):
+            if idx in idxs:
+                info["dscore"] = scores[idx]
+            else:
+                info["dscore"] = None
+
+        return infos
+
+
+class TransformCompose:
+    def __init__(self, transforms):
+        self._transforms = transforms
+
+    def __call__(self, infos: List[dict]):
+        for x in self._transforms:
+            infos = x(infos)
+        return infos
