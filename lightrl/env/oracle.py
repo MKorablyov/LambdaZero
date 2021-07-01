86c16aada512bbf052dc97b5b4b14ad16cbd706c
andreicnica <andreic.nica@gmail.com>
2021-07-01 14:30:19 -0400

    Working live updatable buffer for docking

--- lightrl/env/oracle.py
+++ lightrl/env/oracle.py
@@ -49,12 +49,13 @@ class InterogateOracle:
         If not found use local thread to calculate oracle (+update db)
     """
 
-    def __init__(self, num_workers=4):
+    def __init__(self, num_workers=4, send_updates_conn=None):
         fake_logger = FakeRemoteLog()
         oracle_config["logger"] = fake_logger
         oracle_config["num_threads"] = 0
         dock_oracle = DockingOracle(**oracle_config)
 
+        self._send_updates_conn = send_updates_conn
         self._dock_db = dock_oracle
         self._dock_db.set_fix_none(False)
         self._dock_db.set_norm_data(False)
@@ -74,6 +75,9 @@ class InterogateOracle:
 
         self._oracle_req = dict()
         self._ready_req = dict()
+        self._max_send_pending_que = num_workers * 2
+        self._pending_que = []
+
         # self.mean = oracle_config["mean"]
         # self.std = oracle_config["std"]
         # self.act_y = oracle_config["act_y"]
@@ -93,6 +97,35 @@ class InterogateOracle:
                 # In case we reached max failed hits on a smile we should put None
                 self._ready_req[smi["smiles"]] = true_dock
 
+    def update_pending_que(self, requests=[]):
+        self.empty_recv_que()
+
+        if len(requests) > 0:
+            self._pending_que = requests
+
+        while len(self._oracle_req) < self._max_send_pending_que and len(self._pending_que) > 0:
+            new_req = self._pending_que.pop(0)
+
+            # Double check we do not have score already
+            dockscore, true_dock = self.check_db([new_req])[0]
+            if dockscore is None and new_req["smiles"] not in self._oracle_req:
+                _id = np.random.randint(10000)
+
+                self._oracle_req[new_req["smiles"]] = True
+                self.remote_send.put((_id, new_req["smiles"], new_req))
+
+    def check_db(self, x):
+        # Send only
+        smi_data = [{"smiles": i["smiles"]} for i in x]
+        res, tres = self._dock_db(smi_data, ret_true_dockscore=True)
+        return list(zip(res, tres))
+
+    def empty_recv_que(self):
+        while not self.remote_recv.empty():
+            _id, smiles, ret = self.remote_recv.get()
+            self._dock_db.update_predocked(smiles, ret)
+            self._oracle_req.pop(smiles)
+
     def get_req(self, smiles=None):
         if smiles is not None:
             raise NotImplementedError
@@ -103,6 +136,10 @@ class InterogateOracle:
 
         while len(self._oracle_req) > 0:
             _id, smiles, ret = self.remote_recv.get()
+
+            if self._send_updates_conn is not None:
+                self._send_updates_conn.put((smiles, ret))
+
             self._dock_db.update_predocked(smiles, ret)
             req[smiles] = ret
             self._oracle_req.pop(smiles)
