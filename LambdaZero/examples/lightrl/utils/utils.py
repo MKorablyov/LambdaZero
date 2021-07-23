c82f057a0895ba1071eba6d6430f90de1021d10e
andreicnica <andreic.nica@gmail.com>
2021-07-23 17:18:17 -0400

    Back to old version of seen mol

--- LambdaZero/examples/lightrl/utils/utils.py
+++ LambdaZero/examples/lightrl/utils/utils.py
@@ -260,11 +260,17 @@ class LogTopStats:
 
                 # If new key and respects candidate conditions
                 if good:
+                    # TODO Should consider seen only the mol we actually calculate GT for (oracle d)
+                    self._seen_mol.update([_id])  # TODO Should fix this
+
                     self._new_info.append(info)
                     self._all_time_topk_score.update([info[self._order_key]])
 
     def log(self):
-        logs = dict({f"top{self._topk}_count": len(self._new_info)})
+        logs = dict({
+            f"top{self._topk}_count": len(self._new_info),
+            f"seen_mol": len(self._seen_mol)
+        })
         logs.update(self._all_time_topk_score.log())
 
         if len(self._new_info) > self._topk:
@@ -274,11 +280,12 @@ class LogTopStats:
 
             log_info = [self._new_info[x] for x in topk_idx]  # type: List[dict]
 
-            # Add logged molecules to seen set
-            for info in log_info:
-                _id = info.get(self._unique_key, None)
-                if _id is not None and _id not in self._seen_mol:
-                    self._seen_mol.update([_id])
+            # TODO Should consider seen only the mol we actually calculate GT for
+            # # Add logged molecules to seen set
+            # for info in log_info:
+            #     _id = info.get(self._unique_key, None)
+            #     if _id is not None and _id not in self._seen_mol:
+            #         self._seen_mol.update([_id])
 
             if self._transform_info is not None:
                 log_info = self._transform_info(log_info)
